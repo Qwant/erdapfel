b9d0a2f459c3d67d7a1aa48dfae69f6b465e8fe2
jlaville <j.laville@qwant.com>
2018-06-21 11:39:03 +0200

    remove critical config from config file

--- src/mapbox/style_laundry.js
+++ src/mapbox/style_laundry.js
@@ -5,8 +5,8 @@ function StyleLaundry(style) {
   let rawStyle = JSON.stringify(style)
   return JSON.parse(rawStyle
     .replace(/\{locale\}/g, window.getLang().code)
-    .replace('"{tileserver_base}"', JSON.stringify(mapStyleConfig.tileserver_base))
-    .replace('"{tileserver_poi}"', JSON.stringify(mapStyleConfig.tileserver_poi))
+    .replace('"{tileserver_base}"', mapStyleConfig.tileserver_base)
+    .replace('"{tileserver_poi}"', mapStyleConfig.tileserver_poi)
     .replace('{spriteserver}', mapStyleConfig.spriteserver)
     .replace('{fontserver}', mapStyleConfig.fontserver))
 }
