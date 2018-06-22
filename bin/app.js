4dadddcd088f7ac84a59ae2fa83048e84bc36082
jlaville <j.laville@qwant.com>
2018-06-22 17:15:03 +0200

    refact gettext method

--- bin/app.js
+++ bin/app.js
@@ -3,14 +3,14 @@ const app = express()
 
 function App(config) {
   /* set in res the user lang config */
-  const lang = require('./middlewares/lang')(config.languages)
-  app.use(lang)
+  const userLanguage = require('./middlewares/user_language')(config.languages)
+  app.use(userLanguage)
 
   this.handler = null
   app.set('view engine', 'ejs')
 
   /* initialize gettext with correct dictionary */
-  const gettext = require('./gettext')(app, config.languages.supportedLanguages) /* set _ et _n as locals app methods */
+  const gettext = require('./gettext_wrapper')(app, config.languages.supportedLanguages) /* set _ et _n as locals app methods */
   app.use(gettext)
 
   app.use(express.static(`${__dirname}/../public`))
