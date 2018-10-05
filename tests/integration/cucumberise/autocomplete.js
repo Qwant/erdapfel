bdf95e2af07499e256d26e5ff9a7291ea7a68d81
jlaville <j.laville@qwant.com>
2018-10-05 18:03:42 +0200

    wip cucumbierize autcomplete test

---
+++ tests/integration/cucumberise/autocomplete.js
@@ -0,0 +1,21 @@
+export default class AutocompleteCucumberise {
+  constructor(page) {
+    this.page = page
+  }
+
+  async typeAndWait(word) {
+
+  }
+
+  async getSuggestList() {
+
+  }
+
+  async prepareResponse(response, delay = false) {
+    if(delay) {
+
+    }
+  }
+
+  async
+}
