f9665ed30d6adb358e2e65d87acddfcf4595fb08
Patrick Fratczak <p.fratczak@qwant.com>
2018-10-19 17:05:16 +0200

    QMAPS-534 [ui] Add ScaleControl

--- src/adapters/scene.js
+++ src/adapters/scene.js
@@ -1,4 +1,4 @@
-import {Map, Marker, LngLat} from 'mapbox-gl--ENV'
+import {Map, Marker, LngLat, ScaleControl} from 'mapbox-gl--ENV'
 import PoiPopup from './poi_popup'
 import MobileCompassControl from "../mapbox/mobile_compass_control"
 import ExtendedControl from "../mapbox/extended_nav_control"
@@ -50,6 +50,12 @@ Scene.prototype.initMapBox = function () {
   const interactiveLayers =  ['poi-level-1', 'poi-level-2', 'poi-level-3']
 
   this.mb.on('load', () => {
+    const scaleMetric = new ScaleControl({
+      maxWidth: 72,
+      unit: 'metric'
+    })
+    this.mb.addControl(scaleMetric, 'bottom-right')
+
     const extendedControl = new ExtendedControl()
     const mobileCompassControl = new MobileCompassControl()
 
