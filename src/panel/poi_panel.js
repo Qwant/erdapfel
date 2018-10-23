49718b21b995fb004528cfcd132ee58e3cc9008f
jlaville <j.laville@qwant.com>
2018-10-23 15:10:59 +0200

    update babel core for bridged version & package-lock

--- src/panel/poi_panel.js
+++ src/panel/poi_panel.js
@@ -20,6 +20,7 @@ function PoiPanel(sharePanel) {
   this.PoiBlocContainer = PoiBlocContainer
   this.panel = new Panel(this, PoiPanelView)
   this.sharePanel = sharePanel
+  this.htmlEncode = ExtendedString.htmlEncode
   PanelManager.register(this)
   UrlState.registerResource(this, 'place')
 }
