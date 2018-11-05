4ae3ced88edfc686e4cd898ee7a9df034821b519
Noémie <nlehuby@zaclys.net>
2018-11-05 11:32:03 +0100

    update i18n script (#55)
    let's shuffle the po a little bit

--- i18n.sh
+++ i18n.sh
@@ -2,7 +2,8 @@
 
 set -ev
 find config "views/" -name "*.ejs"| xargs xgettext --language=PHP --force-po -o tt.pot --from-code=UTF-8 -
-find config bin "public/build/javascript/" -regex ".*\.\(yml\|js\)"| xargs xgettext --language=Python --force-po -o tt.pot --from-code=UTF-8 -j -
+find config -name "*.yml"| xargs xgettext --language=Python --force-po -o tt.pot --from-code=UTF-8 -j -
+find bin "public/build/javascript/" -name "*.js"| xargs xgettext --force-po -o tt.pot --from-code=UTF-8 -j -
 
 for i in language/message/*.po; do msgmerge --no-fuzzy-matching --update $i tt.pot  ;done
 
