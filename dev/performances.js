4dee622cd82570f90e43bc8b4dbfe945bcafb03d
julien <jl@gmail.com>
2018-11-26 14:43:54 +0100

    WIP

--- dev/performances.js
+++ dev/performances.js
@@ -1,8 +1,12 @@
 const puppeteer = require('puppeteer')
 
 const getScriptDuration = async () => {
+
+    /* start host */
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
+
+    /* connect to homepage for performance test */
     await page.goto('http://10.100.31.92/tileview/')
 
     const metrics = await page.metrics()
