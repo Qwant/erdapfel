843229ae8f787d28b5ee95f513310fb49ad5950c
jlaville <j.laville@qwant.com>
2018-11-28 11:38:28 +0100

    wip add packet size stats

--- dev/performances.js
+++ dev/performances.js
@@ -1,17 +1,38 @@
 const puppeteer = require('puppeteer')
+const fs = require('fs')
+const webpack = require('webpack')
+const webpackConfig = require('../build/webpack.config')
 
 const getScriptDuration = async () => {
 
-    /* start host */
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
+  /* production build */
+  buildProd()
 
-    /* connect to homepage for performance test */
-    await page.goto('http://10.100.31.92/tileview/')
+  /* start host */
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
 
-    const metrics = await page.metrics()
+  /* connect to homepage for performance test */
+  await page.goto('http://10.100.31.92/tileview/')
 
-    console.log(metrics.ScriptDuration)
+  const metrics = await page.metrics()
+  const mapStats = fs.statSync('../public/build/javascript/map.js')
+  const appStats = fs.statSync('../public/build/javascript/bundle.js')
+
+
+  console.log(mapStats)
+  console.log(appStats)
+
+
+  let reportData = {
+    scriptDuration : metrics.ScriptDuration,
+    size: {
+      app : appStats.size,
+      map: mapStats.size
+    }
+  }
+
+  writeReport(reportData)
 
 
 
@@ -22,3 +43,19 @@ const getScriptDuration = async () => {
 }
 
 getScriptDuration()
+
+
+
+function writeReport(reportData) {
+  fs.writeFileSync('./report.json', JSON.stringify(reportData, null, '\t'))
+}
+
+function buildProd() {
+
+  webpack(webpackConfig, function(error, stats) {
+    if(error)
+      console.error(error)
+    console.log(stats)
+  });
+}
+
