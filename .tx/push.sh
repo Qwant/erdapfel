d2789566a03837a9204510e3067d4a2d72b3b707
nlehuby <n.lehuby@qwant.com>
2019-01-17 12:08:53 +0100

    i18n - only push strings to translate to transifex
    and do not overwrite translations already in transifex but not yet in the project

--- .tx/push.sh
+++ .tx/push.sh
@@ -5,4 +5,4 @@ npm run build -- --mode=production
 npm run i18n
 python3.5 -m pip install --user "urllib3>=1.21.1,<1.24" transifex-client==0.13.5
 sudo echo $'[https://www.transifex.com]\nhostname = https://www.transifex.com\nusername = '"$TRANSIFEX_API_USER"$'\npassword = '"$TRANSIFEX_API_KEY"$'\n' > ~/.transifexrc
-tx push -st
+tx push -s
