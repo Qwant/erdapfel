ccc4110154a063244942baa8223398e8b3856c07
jlaville <j.laville@qwant.com>
2019-01-17 17:15:19 +0100

    add direction basic test

---
+++ tests/__data__/mapbox.js
@@ -0,0 +1,58 @@
+module.exports = {
+  "routes":
+    [{
+      "geometry": {"coordinates": [[2.290454, 48.823566], [2.290454, 48.823566]], "type": "LineString"},
+      "legs": [{
+        "summary": "Rue Paul Lefèvre",
+        "weight": 0,
+        "duration": 0,
+        "steps": [{
+          "intersections": [{"out": 0, "entry": [true], "bearings": [326], "location": [2.290454, 48.823566]}],
+          "driving_side": "right",
+          "geometry": {"coordinates": [[2.290454, 48.823566], [2.290454, 48.823566]], "type": "LineString"},
+          "mode": "driving",
+          "maneuver": {
+            "bearing_after": 326,
+            "bearing_before": 0,
+            "location": [2.290454, 48.823566],
+            "modifier": "straight",
+            "type": "depart",
+            "instruction": "Se diriger vers le nord-ouest sur la rue Paul Lefèvre"
+          },
+          "weight": 0,
+          "duration": 0,
+          "name": "Rue Paul Lefèvre",
+          "distance": 0
+        }, {
+          "intersections": [{"in": 0, "entry": [true], "bearings": [146], "location": [2.290454, 48.823566]}],
+          "driving_side": "right",
+          "geometry": {"coordinates": [[2.290454, 48.823566], [2.290454, 48.823566]], "type": "LineString"},
+          "mode": "driving",
+          "maneuver": {
+            "bearing_after": 0,
+            "bearing_before": 326,
+            "location": [2.290454, 48.823566],
+            "modifier": "straight",
+            "type": "arrive",
+            "instruction": "Vous êtes arrivé à votre destination, droit devant"
+          },
+          "weight": 0,
+          "duration": 0,
+          "name": "Rue Paul Lefèvre",
+          "distance": 0
+        }],
+        "distance": 0
+      }],
+      "weight_name": "routability",
+      "weight": 0,
+      "duration": 0,
+      "distance": 0
+    }], "waypoints":
+    [{
+      "distance": 13.481332582809157,
+      "name": "Rue Paul Lefèvre",
+      "location": [2.290454, 48.823566]
+    }, {"distance": 13.481332582809157, "name": "Rue Paul Lefèvre", "location": [2.290454, 48.823566]}], "code":
+    "Ok", "uuid":
+    "cjr0t9eej0jlk44jy6zmzgtnf"
+}
