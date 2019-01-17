198975114c1d8d129765617ff9b3c51d2dd9d32a
jlaville <j.laville@qwant.com>
2019-01-17 18:12:12 +0100

    update og image path from relative to absolute with new server config WIP wait for png / svg

--- bin/middlewares/og_meta.js
+++ bin/middlewares/og_meta.js
@@ -1,9 +1,6 @@
 const request = require('request')
 
-const ogMetas = [
-  {name : 'site_name', content : 'Qwant Maps'},
-  {name : 'image', content : './statics/images/qwant-logo.svg'}
-]
+
 
 module.exports = function(config) {
   // Use url from server config if defined
@@ -39,7 +36,9 @@ module.exports = function(config) {
   }
 
   function commonMeta(locale, req, res) {
-    res.locals.ogMetas = ogMetas.map(meta => meta)
+    res.locals.ogMetas = []
+    res.locals.ogMetas.push({name : 'site_name', content : 'Qwant Maps'})
+    res.locals.ogMetas.push({name : 'image', content : `${config.server.hostName}${config.system.baseUrl}statics/images/qwant-logo.svg`})
     res.locals.ogMetas.push({name : 'locale', content : locale.locale})
     res.locals.ogMetas.push({name : 'description', content : res.locals. _('The map that respects your privacy')})
   }
