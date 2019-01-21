23eb310e74eea46d2988931a4981241d00306c5f
jlaville <j.laville@qwant.com>
2019-01-21 11:21:30 +0100

    update review  - image size (indexed color)  - add type website  - update hostname

--- bin/middlewares/og_meta.js
+++ bin/middlewares/og_meta.js
@@ -37,8 +37,9 @@ module.exports = function(config) {
 
   function commonMeta(locale, req, res) {
     res.locals.ogMetas = []
+    res.locals.ogMetas.push({name : 'type', content : 'website'})
     res.locals.ogMetas.push({name : 'site_name', content : 'Qwant Maps'})
-    res.locals.ogMetas.push({name : 'image', content : `${config.server.hostName}${config.system.baseUrl}statics/images/qwant_logo_og.png`})
+    res.locals.ogMetas.push({name : 'image', content : `https://${req.get('host')}${config.system.baseUrl}statics/images/qwant_logo_og.png`})
     res.locals.ogMetas.push({name : 'locale', content : locale.locale})
     res.locals.ogMetas.push({name : 'description', content : res.locals. _('The map that respects your privacy')})
   }
