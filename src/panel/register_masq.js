1376d5a1c477e40bf36a1bc948498c0e9c84435e
Sujeeban Thuraisamy <s.thuraisamy@qwant.com>
2019-01-28 15:11:24 +0100

    add signout button when logged in

--- src/panel/register_masq.js
+++ src/panel/register_masq.js
@@ -12,18 +12,39 @@ export default class RegisterMasqPanel {
     if(moduleConfig.name === 'masq') {
       this.store = new Store()
       this.isActive = false
+      this.registering = false
       listen('register_panel__show', () => {
         this.panel.animate(.25, '.register_masq_panel', {top : '100px'})
+      })
+
+      this.isRegistered = false
+      this.store.isRegistered().then((b) => {
+        this.isRegistered = b
+        this.panel.update()
       })
     }
   }
 
   async register() {
+    this.regsitering = true
     try {
       await this.store.register()
+      this.regsitering = false
+      this.isRegistered = await this.store.isRegistered()
     } catch(e) {
       Error.sendOnce('register_masq', 'register', 'error registering masq', e)
+      this.regsitering = false
+      this.isRegistered = await this.store.isRegistered()
     }
     this.panel.animate(.25, '.register_masq_panel', {top : '-300px'})
+    this.panel.update()
+  }
+
+  async unregister() {
+    await this.store.unregister()
+    this.isRegistered = await this.store.isRegistered()
+    window.location.reload()
+    this.panel.update()
+    return
   }
 }
