fece8efc1df45977f30391f9f1b9bfe6479888e1
Sujeeban Thuraisamy <s.thuraisamy@qwant.com>
2019-02-06 18:23:09 +0100

    panel/login_masq.js: loginPanel will not be instantiated when masq disabled

--- src/panel/login_masq.js
+++ src/panel/login_masq.js
@@ -9,27 +9,25 @@ let moduleConfig = nconf.get().store
 export default class LoginMasqPanel {
   constructor() {
     this.panel = new Panel(this, LoginMasqPanelView)
-    if(moduleConfig.name === 'masq') {
-      this.store = new Store()
-      this.isActive = false
-      this.loggingIn = false
-      this.username = null
-      this.profileImage = null
-      listen('login_panel__show', () => {
-        this.panel.animate(.25, '.login_masq_panel', {top : '100px'})
-      })
+    this.store = new Store()
+    this.isActive = false
+    this.loggingIn = false
+    this.username = null
+    this.profileImage = null
+    listen('login_panel__show', () => {
+      this.panel.animate(.25, '.login_masq_panel', {top : '100px'})
+    })
 
-      this.isLoggedIn = false
-      this.store.isLoggedIn().then(async (b) => {
-        this.isLoggedIn = b
-        if (this.isLoggedIn) {
-          const { username, profileImage } = await this.store.getUserInfo()
-          this.username = username
-          this.profileImage = profileImage
-        }
-        this.panel.update()
-      })
-    }
+    this.isLoggedIn = false
+    this.store.isLoggedIn().then(async (b) => {
+      this.isLoggedIn = b
+      if (this.isLoggedIn) {
+        const { username, profileImage } = await this.store.getUserInfo()
+        this.username = username
+        this.profileImage = profileImage
+      }
+      this.panel.update()
+    })
   }
 
   async login() {
