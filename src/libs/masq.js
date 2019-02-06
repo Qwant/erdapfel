bdc2d8255adcc670ffe99bbbb7d6741a3f8ea1eb
Sujeeban Thuraisamy <s.thuraisamy@qwant.com>
2019-02-06 18:12:05 +0100

    libs/masq.js: reformat to es6 syntax

--- src/libs/masq.js
+++ src/libs/masq.js
@@ -1,116 +1,115 @@
-const Masq = require('masq-lib')
+import Masq from 'masq-lib'
 
 const Error = require('../adapters/error').default
 const handleError = (fct, msg, e) => {
   Error.sendOnce('masq_store', fct, msg, e)
 }
 
-function MasqStore(config) {
-  this.masq = null
-  this.loginLink = null
-  this.config = config
-}
+export default class MasqStore {
+  constructor(config) {
+    this.masq = null
+    this.loginLink = null
+    this.config = config
+  }
 
-MasqStore.prototype.getAllPois = async function() {
-  const list = await this.masq.list()
+  async getAllPois() {
+    const list = await this.masq.list()
 
-  const filteredKeys = Object.keys(list).reduce((filtered, k) => {
-    if (Poi.isPoiCompliantKey(k)) {
-      filtered.push(k)
+    const filteredKeys = Object.keys(list).reduce((filtered, k) => {
+      if (Poi.isPoiCompliantKey(k)) {
+        filtered.push(k)
+      }
+      return filtered
+    }, [])
+
+    if (filteredKeys.length === 0) {
+      return []
     }
-    return filtered
-  }, [])
 
-  if (filteredKeys.length === 0) {
-    return []
+    const valuePromises = filteredKeys.map(k => {
+      return this.masq.get(k)
+    })
+
+    try {
+      const values = await Promise.all(valuePromises)
+      return values
+    } catch (e) {
+      handleError('getAllPois', 'error getting pois', e)
+      throw e
+    }
   }
 
-  const valuePromises = filteredKeys.map(k => {
-    return this.masq.get(k)
-  })
-
-  try {
-    const values = await Promise.all(valuePromises)
-    return values
-  } catch (e) {
-    handleError('getAllPois', 'error getting pois', e)
-    throw e
-  }
-}
-
-MasqStore.prototype.getUserInfo = async function() {
-  const username = await this.masq.getUsername()
-  const profileImage = await this.masq.getProfileImage()
-  return {
-    username,
-    profileImage
-  }
-}
-
-MasqStore.prototype.isLoggedIn = async function() {
-  return await Promise.resolve(this.masq && this.masq.isLoggedIn())
-}
-
-MasqStore.prototype.login = async function(apps) {
-  // connect to Masq
-  window.open(this.loginLink)
-  await this.masq.logIntoMasq(false)
-}
-
-MasqStore.prototype.logout = async function() {
-  await this.masq.signout()
-}
-
-MasqStore.prototype.onConnect = async function () {
-  // executed when maps is opened
-  this.masq = new Masq(this.config.title, this.config.desc, this.config.icon)
-  if (this.masq.isLoggedIn()) {
-    await this.masq.connectToMasq()
-  } else {
-    this.loginLink = await this.masq.getLoginLink()
+  async getUserInfo() {
+    const username = await this.masq.getUsername()
+    const profileImage = await this.masq.getProfileImage()
+    return {
+      username,
+      profileImage
+    }
   }
 
-  return
-}
+  async isLoggedIn() {
+    return await Promise.resolve(this.masq && this.masq.isLoggedIn())
+  }
 
-MasqStore.prototype.has = async function(k) {
-  return !!(await this.get(k))
-}
+  async login(apps) {
+    // connect to Masq
+    window.open(this.loginLink)
+    await this.masq.logIntoMasq(false)
+  }
 
-MasqStore.prototype.get = async function(k) {
-  console.log('[MasqStore.get]')
-  try {
-    const value = await this.masq.get(k)
-    return value
-  } catch (e) {
-    handleError('get', `error parsing item with key ${k}`, e)
-    throw e
+  async logout() {
+    await this.masq.signout()
+  }
+
+  async onConnect () {
+    // executed when maps is opened
+    this.masq = new Masq(this.config.title, this.config.desc, this.config.icon)
+    if (this.masq.isLoggedIn()) {
+      await this.masq.connectToMasq()
+    } else {
+      this.loginLink = await this.masq.getLoginLink()
+    }
+
+    return
+  }
+
+  async has(k) {
+    return !!(await this.get(k))
+  }
+
+  async get(k) {
+    try {
+      const value = await this.masq.get(k)
+      return value
+    } catch (e) {
+      handleError('get', `error parsing item with key ${k}`, e)
+      throw e
+    }
+  }
+
+  async set(k, v) {
+    try {
+      await this.masq.put(k, v)
+    } catch (e) {
+      handleError('set', 'error setting item', e)
+      throw e
+    }
+    return
+  }
+
+  async clear() {
+    handleError('clear', 'masq storage doesn\'t support clear method', e)
+    return Promise.resolve()
+  }
+
+  async del(k) {
+    try {
+      await this.masq.del(k)
+    } catch (e) {
+      handleError('del', 'error removing item', e)
+      throw e
+    }
+    return
   }
 }
-
-MasqStore.prototype.set = async function(k, v) {
-  try {
-    await this.masq.put(k, v)
-  } catch (e) {
-    handleError('set', 'error setting item', e)
-    throw e
-  }
-  return
-}
-
-MasqStore.prototype.clear = function() {
-  handleError('clear', 'masq storage doesn\'t support clear method', e)
-  return Promise.resolve()
-}
-
-MasqStore.prototype.del = async function(k) {
-  try {
-    await this.masq.del(k)
-  } catch (e) {
-    handleError('del', 'error removing item', e)
-    throw e
-  }
-  return
-}
-
-module.exports = MasqStore
