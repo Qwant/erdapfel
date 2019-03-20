0ef26fc800e0081ea57f7eacf9aa2d62da90fbd6
Sujeeban Thuraisamy <s.thuraisamy@qwant.com>
2019-03-20 15:14:00 +0100

    masq: do not display old login panel

--- src/panel/app_panel.js
+++ src/panel/app_panel.js
@@ -1,7 +1,6 @@
 import PanelsView from '../views/app_panel.dot'
 import Panel from '../libs/panel'
 import FavoritePanel from "./favorites_panel";
-import LoginMasqPanel from "./login_masq";
 import PoiPanel from "./poi_panel";
 import ServicePanel from './service_panel';
 import Share from "../modals/share"
@@ -15,7 +14,6 @@ import Menu from "./menu";
 
 const performanceEnabled = nconf.get().performance.enabled
 const directionEnabled = nconf.get().direction.enabled
-const masqEnabled = nconf.get().masq.enabled
 import Telemetry from "../libs/telemetry";
 
 
@@ -34,10 +32,6 @@ function AppPanel(parent) {
     this.directionPanel = new DirectionPanel()
   }
 
-  this.masqEnabled = masqEnabled
-  if (this.masqEnabled) {
-    this.masqPanel = new LoginMasqPanel()
-  }
   this.panel = new Panel(this, PanelsView, parent)
   this.geolocationModal = new GeolocationModal()
   this.geolocationDeniedModal = new GeolocationDeniedModal()
