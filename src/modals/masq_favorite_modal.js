6e6c6fcf6403dc5547b273ac8e0ccaeafb74d42c
Sujeeban Thuraisamy <s.thuraisamy@qwant.com>
2019-03-22 16:27:06 +0100

    modals/masq_favorite_modal.js: add do-not-ask-again checkbox

--- src/modals/masq_favorite_modal.js
+++ src/modals/masq_favorite_modal.js
@@ -2,6 +2,7 @@ import MasqFavoriteModalView from '../views/masq_favorite_modal.dot'
 import Modal from "./modal"
 import nconf from "@qwant/nconf-getter"
 import Store from "../adapters/store"
+import {version} from '../../config/constants.yml'
 
 const store = new Store()
 
@@ -19,16 +20,19 @@ export default class MasqFavoriteModal {
 
 
   open () {
-    this.modal.open()
+    let doNotAskAgain = localStorage.getItem(`qmaps_v${version}_add_favorite_ask_masq`)
+    if (doNotAskAgain === "false" || doNotAskAgain === null) {
+      this.modal.open()
+    }
   }
 
   close () {
-    console.log(document.getElementById("doNotAsk").checked)
+    const doNotAskAgain = document.getElementById("doNotAsk").checked
+    localStorage.setItem(`qmaps_v${version}_add_favorite_ask_masq`, doNotAskAgain)
     this.modal.close()
   }
 
   async openMasq () {
-    console.log('login')
     await store.login()
     if (store.isLoggedIn) {
       this.close()
