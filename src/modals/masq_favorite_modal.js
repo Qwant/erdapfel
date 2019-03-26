97fad49454ff377a9f434c2cc97752b2e2c7d2b7
Sujeeban Thuraisamy <s.thuraisamy@qwant.com>
2019-03-26 11:22:21 +0100

    masq: add way to wait for favorite modal to close

--- src/modals/masq_favorite_modal.js
+++ src/modals/masq_favorite_modal.js
@@ -16,12 +16,17 @@ export default class MasqFavoriteModal {
     window.__masqFavoriteModal = this
 
     this.modal = new Modal(this, MasqFavoriteModalView)
+
+    this.eventTarget = document.createElement('masqFavoriteModal')
+
+    this.opened = false
   }
 
 
   open () {
     let doNotAskAgain = localStorage.getItem(`qmaps_v${version}_add_favorite_ask_masq`)
     if (doNotAskAgain === "false" || doNotAskAgain === null) {
+      this.opened = true
       this.modal.open()
     }
   }
@@ -30,12 +35,23 @@ export default class MasqFavoriteModal {
     const doNotAskAgain = document.getElementById("doNotAsk").checked
     localStorage.setItem(`qmaps_v${version}_add_favorite_ask_masq`, doNotAskAgain)
     this.modal.close()
+    this.eventTarget.dispatchEvent(new Event('closed'))
+    this.opened = false
   }
 
   async openMasq () {
     await store.login()
-    if (store.isLoggedIn) {
+    if (store.isLoggedIn()) {
       this.close()
     }
+  }
+
+  waitForClose() {
+    if (!this.opened) {
+      return Promise.resolve()
+    }
+    return new Promise((resolve) => {
+      this.eventTarget.addEventListener('closed', resolve, {once: true})
+    })
   }
 }
