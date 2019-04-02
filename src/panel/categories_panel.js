ec5c32493191f6436405fe078e90ef7a875f082d
jlaville <j.laville@qwant.com>
2019-04-02 10:44:37 +0200

    WIP add fullfill for test

--- src/panel/categories_panel.js
+++ src/panel/categories_panel.js
@@ -1,14 +1,23 @@
 import Panel from "../libs/panel";
 import panelView from "../views/categories_panel.dot"
 import MinimalHourPanel from "./poi_bloc/opening_minimal";
+import Ajax from "../libs/ajax";
 
 export class CategoriesPanel {
   constructor() {
     this.minimalHourPanel = new MinimalHourPanel()
     this.panel = new Panel(this, panelView)
 
-
+    this.pois = []
     this.isOpen = false
 
+  }
+
+  async fullFill() {
+    let bbox = window.mapGetBounds()
+    console.log(bbox)
+
+    this.pois = await Ajax.get('https://maps.dev.qwant.ninja/maps/geocoder/places_list/', {bbox : bbox,size : '', category : 'leisure'})
+    this.panel.update()
   }
 }
