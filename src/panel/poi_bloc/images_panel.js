c92586118b3ef4852b07fb5ae066396dd105f005
xem <maxime.euziere@gmail.com>
2019-04-10 10:10:34 +0200

    QMAPS-759 MR fixes

--- src/panel/poi_bloc/images_panel.js
+++ src/panel/poi_bloc/images_panel.js
@@ -1,14 +1,14 @@
 import ImagesPanelView from '../../views/poi_bloc/images.dot'
 import Panel from "../../libs/panel";
 
-function ImagesPanel(block) {
-  this.topImageUrl = null
-  this.panel = new Panel(this, ImagesPanelView)
-  this.name = block.name
-  this.block = block
-  if(this.block && this.block.images && this.block.images[0]){
-    this.topImageUrl = this.block.images[0].url
-  }
+export default class ImagesPanel {
+    constructor(block){
+      this.topImageUrl = null
+      this.panel = new Panel(this, ImagesPanelView)
+      this.name = block.name
+      this.block = block
+      if(this.block && this.block.images && this.block.images[0]){
+        this.topImageUrl = this.block.images[0].url
+      }
+    }
 }
-
-export default ImagesPanel
