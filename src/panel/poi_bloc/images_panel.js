83fdc803b279cd18f79664752a3f36860fa50c1c
xem <maxime.euziere@gmail.com>
2019-04-10 09:36:35 +0200

    QMAPS-759 MR fixes

--- src/panel/poi_bloc/images_panel.js
+++ src/panel/poi_bloc/images_panel.js
@@ -1,18 +1,14 @@
 import ImagesPanelView from '../../views/poi_bloc/images.dot'
-import nextTransitionPartial from '../../views/poi_bloc/hour_next_transition_partial.dot'
 import Panel from "../../libs/panel";
-import OsmSchedule from '../../../src/adapters/osm_schedule'
-import Telemetry from "../../libs/telemetry";
 
-function ImagesPanel(block, poi) {
-
-  console.log(block);
-  this.nextTransitionPartial = nextTransitionPartial
+function ImagesPanel(block) {
+  this.topImageUrl = null
   this.panel = new Panel(this, ImagesPanelView)
   this.name = block.name
   this.block = block
-  this.latLng = poi.latLon
-  this.isCollapsed = true
+  if(this.block && this.block.images && this.block.images[0]){
+    this.topImageUrl = this.block.images[0].url
+  }
 }
 
 export default ImagesPanel
