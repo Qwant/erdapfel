d1fb9975126ea15114c8718ad904a93ea80cd4db
Adrien M <a@matissart.net>
2019-04-20 13:54:40 +0200

    Prevent rebuilding existing map fonts on 'npm install'

---
+++ build/build_map_fonts.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+const child_process = require('child_process')
+const path = require('path')
+
+const sync = function(cmd, args, cwd){
+  return child_process.spawnSync(cmd, args, {
+    cwd: cwd,
+    stdio: [process.stdin, process.stdout, process.stderr]
+  })
+}
+
+const fontsPackagePath = path.join('node_modules', 'klokantech-noto-sans')
+const fontsVersion = JSON.parse(fs.readFileSync(path.join(fontsPackagePath, 'package.json')))['_id']
+const generatedVersionFile = path.join(fontsPackagePath, '_output', '.generated')
+
+const generateFonts = function(){
+  sync('node', ['generate.js'], fontsPackagePath)
+  if(fontsVersion){
+    fs.writeFileSync(generatedVersionFile, JSON.stringify({version: fontsVersion}))
+    console.log('Map fonts generated. Version: ', fontsVersion)
+  }
+}
+
+const needsGeneration = function() {
+  if (!fontsVersion) {
+    console.log('Could not find map fonts version')
+  }
+  if (fontsVersion && fs.existsSync(generatedVersionFile)){
+    let generatedVersion = JSON.parse(fs.readFileSync(generatedVersionFile)).version
+    if (generatedVersion === fontsVersion){
+      console.log('Map fonts are up to date. Version:', fontsVersion)
+      return false
+    }
+    else {
+      console.log('Map fonts are not up to date. Found:', generatedVersion)
+      console.log('Map fonts version to generate: ', fontsVersion)
+    }
+  }
+  return true
+}
+
+if (needsGeneration()){
+  generateFonts()
+}
