2e916a7c08b0a18fb247097cd1cb021b376ed2a6
xem <maxime.euziere@gmail.com>
2019-05-20 16:51:47 +0200

    QMAPS-zoom-fixes: avoid JS error on PJ dezoom (todo: catch errors 400 and zoom on current geoloc or Paris with zoom = 11)

--- src/adapters/scene_category.js
+++ src/adapters/scene_category.js
@@ -18,20 +18,22 @@ export default class SceneCategory {
 
   addCategoryMarkers(pois) {
     this.setOsmPoisVisibility(false)
-    pois.forEach(poi => {
-      const { id, name, className, subClassName, type, latLon } = poi
-      const marker = createIcon({ className, subClassName, type }, name, true)
-      poi.marker_id = `marker_${id}`
-      marker.onclick = function(){
-        fire('click_category_poi', poi)
-      }
-      marker.id = poi.marker_id
-      this.markers.push(
-        new Marker({ element: marker })
-         .setLngLat(latLon)
-           .addTo(this.map)
-      )}
-    )
+    if(pois) {
+      pois.forEach(poi => {
+        const {id, name, className, subClassName, type, latLon} = poi
+        const marker = createIcon({className, subClassName, type}, name, true)
+        poi.marker_id = `marker_${id}`
+        marker.onclick = function () {
+          fire('click_category_poi', poi)
+        }
+        marker.id = poi.marker_id
+        this.markers.push(
+          new Marker({element: marker})
+            .setLngLat(latLon)
+            .addTo(this.map)
+        )}
+      )
+    }
   }
 
   resetMarkers() {
