e729db6ff797d9f68177db8ff743854ac40ce35e
JVQ <jv.quilichini@gmail.com>
2019-06-05 16:09:37 +0200

    [QMAPS-815] Try fix tests

--- tests/integration/tests/autocomplete.js
+++ tests/integration/tests/autocomplete.js
@@ -83,7 +83,7 @@ test('keyboard navigation', async () => {
   expect(searchValue.trim()).toEqual(expectedLabelName)
 
   /* got to last item */
-  for(let i = 0; i < autocomplete.suggest.max_items - 2; i++) {
+  for(let i = 0; i < autocomplete.suggest.max_items - 3; i++) {
     await autocompleteHelper.pressDown()
   }
  /* one step more */
