8a41982f7a839bc1b0243eb80267e8bee6e3ca38
Sujeeban Thuraisamy <s.thuraisamy@qwant.com>
2019-06-18 12:18:32 +0200

    Masq: Display avatar of the user instead of the burger when logged in

--- src/panel/menu.js
+++ src/panel/menu.js
@@ -4,6 +4,7 @@ import constants from '../../config/constants.yml';
 import LoginMasqPanel from './login_masq';
 import SearchInput from '../ui_components/search_input';
 import nconf from '../../local_modules/nconf_getter';
+import Store from '../adapters/store';
 
 export default class Menu {
   constructor() {
@@ -12,13 +13,55 @@ export default class Menu {
     this.menuItems = constants.menu;
     this.isDirectionActive = nconf.get().direction.enabled;
 
+    this.menuInitialized = true;
+
     this.isMasqEnabled = nconf.get().masq.enabled;
     if (this.isMasqEnabled) {
+      this.menuInitialized = false;
       this.masqPanel = new LoginMasqPanel();
 
-      this.initPromise = this.masqPanel.init().then(() => {
-        this.panel.update();
+      this.store = new Store();
+
+      this.username = null;
+      this.profileImage = null;
+
+      this.store.onToggleStore(async() => {
+        this.isLoggedIn = await this.store.isLoggedIn();
+        if (this.isLoggedIn) {
+          const userInfo = await this.store.getUserInfo();
+          this.username = userInfo.username;
+          this.profileImage = userInfo.profileImage;
+        }
+        await this.updateAndKeepState();
       });
+
+      this.isLoggedIn = false;
+
+      this.initPromise = this.store.isLoggedIn().then(async(b) => {
+        this.isLoggedIn = b;
+
+        if (this.isLoggedIn) {
+          const userInfo = await this.store.getUserInfo();
+          this.username = userInfo.username;
+          this.profileImage = userInfo.profileImage;
+        }
+      });
+
+      Promise.all([this.initPromise, this.masqPanel.init()]).then(async() => {
+        this.menuInitialized = true;
+        await this.updateAndKeepState();
+      });
+    }
+  }
+
+  async updateAndKeepState() {
+    this.panel.update();
+    if (this.isOpen) {
+      await Promise.all([
+        this.panel.addClassName(.3, '.menu__panel', 'menu__panel--active'),
+        this.panel.addClassName(0, '.menu__overlay', 'menu__overlay--active'),
+        this.panel.addClassName(.6, '.menu__overlay', 'menu__overlay--fade_active'),
+      ]);
     }
   }
 
@@ -35,9 +78,6 @@ export default class Menu {
   }
 
   async open() {
-    if (this.initPromise) {
-      await this.initPromise;
-    }
     this.isOpen = true;
 
     await Promise.all([
@@ -51,7 +91,6 @@ export default class Menu {
     this.isOpen = false;
     await Promise.all([
       this.panel.removeClassName(.3, '.menu__panel', 'menu__panel--active'),
-
       this.panel.removeClassName(.6, '.menu__overlay', 'menu__overlay--fade_active'),
       this.panel.removeClassName(0, '.menu__overlay', 'menu__overlay--active'),
     ]);
