c4d6745a2b07e135a2cd994d750999f19ad694c7
Sujeeban Thuraisamy <s.thuraisamy@qwant.com>
2019-06-19 16:16:53 +0200

    Masq: make onboarding first page one of the steps

--- src/modals/masq_onboarding_modal.js
+++ src/modals/masq_onboarding_modal.js
@@ -18,7 +18,6 @@ export default class MasqOnboardingModal {
     this.eventTarget = document.createElement('masqOnboardingModal');
 
     this.opened = false;
-    this.explain = false;
     this.step = 1;
   }
 
@@ -32,7 +31,6 @@ export default class MasqOnboardingModal {
     this.modal.close();
     this.eventTarget.dispatchEvent(new Event('closed'));
     this.opened = false;
-    this.explain = false;
   }
 
   async openMasq() {
@@ -53,15 +51,9 @@ export default class MasqOnboardingModal {
     });
   }
 
-  async openSteps() {
-    this.explain = true;
-    this.step = 1;
-    this.modal.panel.update();
-  }
-
   async previousStep() {
     if (this.step === 1) {
-      this.step = 3;
+      this.step = 4;
     } else {
       this.step = this.step - 1;
     }
@@ -69,7 +61,7 @@ export default class MasqOnboardingModal {
   }
 
   async nextStep() {
-    if (this.step === 3) {
+    if (this.step === 4) {
       this.step = 1;
     } else {
       this.step = this.step + 1;
