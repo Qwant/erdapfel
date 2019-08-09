ec44e379e59c39439ee2c808108df97f3dde517a
bbecquet <benjamin.becquet@gmail.com>
2019-08-09 17:36:00 +0200

    Migrate ServicePanel to react

--- src/panel/service_panel.js
+++ src/panel/service_panel.js
@@ -1,47 +1,72 @@
-import ServicePanelView from '../views/service_panel.dot';
-import Panel from '../libs/panel';
-import nconf from '@qwant/nconf-getter';
+/* globals _ */
+import React from 'react';
+import ReactDOM from 'react-dom';
 import CategoryService from '../adapters/category_service';
 
+const ServicePanel_ = ({ categories, navTo }) =>
+  <div className="service_panel service_panel--active">
+    <h3 className="service_panel__title">
+      {_('Qwant Maps services', 'service panel')}
+    </h3>
+
+    {/* TODO: use real <a> links */}
+    <div
+      className="service_panel__item service_panel__item__fav"
+      onClick={() => navTo('/favs') }
+    >
+      <button className="service_panel__item__action">
+        <i className="icon-icon_star side_bar__item__icon"></i>
+        <span className="service_panel__item__text">{_('YOUR FAVORITES', 'service panel')}</span>
+      </button>
+    </div>
+
+    <div
+      className="service_panel__item service_panel__item__direction"
+      onClick={() => navTo('/routes') }
+    >
+      <button className="service_panel__item__action">
+        <i className="icon-corner-up-right side_bar__item__icon"></i>
+        <span className="service_panel__item__text">{_('DIRECTIONS', 'service panel')}</span>
+      </button>
+    </div>
+
+    {categories.length > 0 && <div className="service_panel__categories">
+      {categories.map(category =>
+        <button
+          key={category.label} className="service_panel__category" type="button"
+          onClick={() => navTo(`/places/?type=${category.name}`) }
+        >
+          <div className="service_panel__category__icon" style={{
+            background: category.backgroundColor,
+          }}>
+            <span className={`icon icon-${category.iconName}`} />
+          </div>
+          <div className="service_panel__category__title">{category.label}</div>
+        </button>)
+      }
+    </div>}
+  </div>;
+
+// Just a facade component to make the dot<=>React link
+// Won't be needed when all panels are migrated
 export default class ServicePanel {
   constructor() {
-    this.panel = new Panel(this, ServicePanelView);
     this.categories = CategoryService.getCategories();
-    this.mustDeployCategories = this.categories.length > 8;
-    this.isDeployed = false;
-    this.isDirectionActive = nconf.get().direction.enabled;
-    this.active = true;
-  }
-
-  toggleCategories() {
-    this.isDeployed = !this.isDeployed;
-    this.panel.update();
-  }
-
-  openFavorite() {
-    window.app.navigateTo('/favs');
-  }
-
-  openDirection() {
-    if (this.isDirectionActive) {
-      window.app.navigateTo('/routes/');
-    }
   }
 
   open() {
-    this.active = true;
-    this.panel.update();
+    ReactDOM.render(
+      <ServicePanel_
+        categories={this.categories}
+        navTo={url => {
+          window.app.navigateTo(url);
+        }}
+      />,
+      document.querySelector('.service_panel__container')
+    );
   }
 
   close() {
-    if (!this.active) {
-      return;
-    }
-    this.active = false;
-    this.panel.update();
-  }
-
-  openCategory(category) {
-    window.app.navigateTo(`/places/?type=${category.name}`);
+    ReactDOM.unmountComponentAtNode(document.querySelector('.service_panel__container'));
   }
 }
