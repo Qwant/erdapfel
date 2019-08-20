58a4eda3956002bc69c38f7ca6d4bd20cb2928f6
bbecquet <benjamin.becquet@gmail.com>
2019-08-20 16:06:14 +0200

    Remove obsolete string ellipsis function

--- src/libs/string.js
+++ src/libs/string.js
@@ -1,23 +1,6 @@
 function ExtendedString() {}
 
 /**
- *
- * @param str
- * @param length
- * @param options : ignoreEllipsis / ignore the ellipsis symbol if the string is shirtened
- * return if str longer than
- */
-ExtendedString.ellipsis = function(str, length, options = {}) {
-  let ellipsis = ' $1&hellip;';
-  if (options.ignoreEllipsis) {
-    ellipsis = ' $1';
-  }
-  return str && str.length > length &&
-    (str = str.slice(0, length).replace(/ ([^ ]*) [^ ]* ?$/, ellipsis)),
-  str && -1 === str.indexOf(' ') && str.length > length && (str += ellipsis),
-  str;
-};
-/**
  * Find the position of the first occurrence of a substring in a string ignoring case
  * @param haystack The string to search in.
  * @param needle
