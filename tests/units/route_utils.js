69e319753cc28af423450a39cc331b2413acd69e
bbecquet <benjamin.becquet@gmail.com>
2019-09-03 18:16:45 +0200

    Use unicode nob space nstead of HTML entitty

--- tests/units/route_utils.js
+++ tests/units/route_utils.js
@@ -1,14 +1,15 @@
+/* eslint-disable no-irregular-whitespace */
 import { formatDuration, formatDistance } from '../../src/libs/route_utils';
 
 describe('route_utils', () => {
   describe('formatDuration', () => {
     const cases = [
-      { seconds: 0, result: '1&nbsp;min' },
-      { seconds: 37, result: '1&nbsp;min' },
-      { seconds: 125, result: '2&nbsp;min' },
-      { seconds: 3600, result: '1&nbsp;h' },
-      { seconds: 5100, result: '1&nbsp;h&nbsp;25&nbsp;min' },
-      { seconds: 36000, result: '10&nbsp;h' },
+      { seconds: 0, result: '1 min' },
+      { seconds: 37, result: '1 min' },
+      { seconds: 125, result: '2 min' },
+      { seconds: 3600, result: '1 h' },
+      { seconds: 5100, result: '1 h 25 min' },
+      { seconds: 36000, result: '10 h' },
     ];
 
     cases.map(({ seconds, result }) =>
@@ -21,11 +22,11 @@ describe('route_utils', () => {
   describe('formatDistance', () => {
     const cases = [
       { meters: 0, result: '' },
-      { meters: 15, result: '15&nbsp;m' },
-      { meters: 500, result: '500&nbsp;m' },
-      { meters: 1234, result: '1,2&nbsp;km' },
-      { meters: 9999, result: '10,0&nbsp;km' },
-      { meters: 123456, result: '123&nbsp;km' },
+      { meters: 15, result: '15 m' },
+      { meters: 500, result: '500 m' },
+      { meters: 1234, result: '1,2 km' },
+      { meters: 9999, result: '10,0 km' },
+      { meters: 123456, result: '123 km' },
     ];
 
     cases.map(({ meters, result }) =>
