a909fcb6bfeeb140db3c61765fae2fcbdcab9b38
bbecquet <benjamin.becquet@gmail.com>
2019-10-23 14:06:48 +0200

    Category: Fill POI category item component

--- src/panel/category/PoiCategoryItem.jsx
+++ src/panel/category/PoiCategoryItem.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import OpeningHour from 'src/components/OpeningHour';
+import ReviewScore from 'src/components/ReviewScore';
+import PhoneBlock from './PhoneBlock';
+import poiSubClass from 'src/mapbox/poi_subclass';
+import PoiTitleImage from 'src/panel/poi/PoiTitleImage';
 
 const PoiCategoryItem = ({ poi }) => {
+  const reviews = poi.blocksByType.grades;
+  const phoneBlock = poi.blocksByType.phone;
+  const address = poi.address || {};
+
   return <div className="category__panel__item">
+    <PoiTitleImage poi={poi} />
+
     <h3 className="category__panel__name">{poi.getInputValue()}</h3>
+
+    {poi.subClassName && <p className="category__panel__type">{poiSubClass(poi.subClassName)}</p>}
+
+    {address.label && <p className="category__panel__address">{address.label}</p>}
+
+    {reviews && <ReviewScore reviews={reviews} poi={poi} />}
+
+    <OpeningHour poi={poi} />
+
+    {phoneBlock && <PhoneBlock phoneBlock={phoneBlock} />}
   </div>;
 };
 
