f3a1d7f167dbf1cb7fd484fd56fe30945bfd0b34
Adrien M <a@matissart.net>
2019-10-28 08:48:04 +0100

    Fix services route regex, to be joined correctly with baseUrl

--- src/panel/app_panel.js
+++ src/panel/app_panel.js
@@ -123,7 +123,7 @@ export default class AppPanel {
     });
 
     // Default matching route
-    this.router.addRoute('Services', '(?:/?)', (_, options = {}) => {
+    this.router.addRoute('Services', '/?', (_, options = {}) => {
       this.resetLayout();
       if (options.focusSearch) {
         SearchInput.select();
