07e1afb48e0305a233a01a3dcea6183f20be7d84
Guillaume Gomez <guillaume1.gomez@gmail.com>
2019-10-31 18:07:36 +0100

    migrate accessibility block to react

--- src/panel/poi_bloc/poi_bloc_container.js
+++ src/panel/poi_bloc/poi_bloc_container.js
@@ -1,14 +1,29 @@
+/* global require */
 import Panel from '../../libs/panel';
 import PoiBlocContainerView from '../../views/poi_bloc/poi_bloc_container.dot';
 import constants from 'config/constants.yml';
+import React from 'react';
+import renderStaticReact from 'src/libs/renderStaticReact';
 
 function PoiBlocContainer() {}
 
 PoiBlocContainer.initBlockComponents = function() {
   PoiBlocContainer.blockComponents = constants.pois.reduce((accBlocks, poiBlock) => {
+    let builder;
+    try {
+      builder = require(`./${poiBlock.panelName}_panel`);
+    } catch (err) {
+      const name = poiBlock.panelName.charAt(0).toUpperCase() + poiBlock.panelName.slice(1);
+      const el = require(`../../views/poi_bloc/${name}`);
+      builder = {
+        default: (block, poi, options) =>
+          renderStaticReact(<el.default block={block} poi={poi} options={options} />),
+        func: true,
+      };
+    }
     accBlocks[poiBlock.apiName] = {
       /* eslint-disable-next-line */
-      poiBlockConstructor: require(`./${poiBlock.panelName}_panel`),
+      poiBlockConstructor: builder,
       options: poiBlock.options,
     };
     return accBlocks;
@@ -43,7 +58,7 @@ PoiBlocContainer.setBlock = function(block) {
 PoiBlocContainer.renderBlock = function(block) {
   const blockComponent = getBlockComponent(block);
   if (blockComponent) {
-    return blockComponent.render();
+    return blockComponent.render ? blockComponent.render() : blockComponent;
   }
   console.warn(`info : component missing (${block.type})`);
 };
@@ -62,6 +77,13 @@ PoiBlocContainer.toString = function(blocks) {
 function getBlockComponent(block) {
   const blockComponent = PoiBlocContainer.blockComponents[block.type];
   if (blockComponent) {
+    if (blockComponent.poiBlockConstructor.func === true) {
+      return blockComponent.poiBlockConstructor.default(
+        block,
+        PoiBlocContainer.poi,
+        blockComponent.options,
+      );
+    }
     return new blockComponent.poiBlockConstructor.default(
       block,
       PoiBlocContainer.poi,
