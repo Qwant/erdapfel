f5e10ae41f46a5fb8f80878a71509a0b198a56d7
Guillaume Gomez <guillaume1.gomez@gmail.com>
2019-11-05 16:03:30 +0100

    Migrate brewery block to react

---
+++ src/views/poi_bloc/Brewery.jsx
@@ -0,0 +1,24 @@
+/* global _ */
+import React from 'react';
+
+const BreweryBlock = ({
+  block,
+  asString,
+}) => {
+  const beers = block.beers;
+  if (asString) {
+    return `${_('Beers')} : ${beers.map(beer => beer.name).join(' - ')}`;
+  }
+
+  return <div>
+    <h6 className="poi_panel__sub__sub_block__title">{ _('Beers', 'poi') }</h6>
+    <ul>
+      {beers.map(beer =>
+        <li className="poi_panel__info__item poi_panel__info__item--beer" key={beer.name}>
+          {beer.name}
+        </li>)}
+    </ul>
+  </div>;
+};
+
+export default BreweryBlock;
