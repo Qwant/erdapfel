824cefc362bc1e8d1c5ecd6e708713d0f0a0e576
xem <maxime.euziere@gmail.com>
2019-11-06 16:56:45 +0100

    QMAPS-1046: add close button and callback (telemetry to rename)

--- src/panel/EventListPanel.jsx
+++ src/panel/EventListPanel.jsx
@@ -2,10 +2,22 @@
 import React from 'react';
 import Panel from 'src/components/ui/Panel';
 import nconf from '@qwant/nconf-getter';
+import Telemetry from "../libs/telemetry";
 
 class EventListPanel extends React.Component {
+
+  close = () => {
+    Telemetry.add(Telemetry.FAVORITE_CLOSE);
+    window.app.navigateTo('/');
+  };
+
   render() {
-    return <Panel resizable title='Hi' minimizedTitle='Hi' className='events_list'>
+    return <Panel
+      resizable
+      title='Hi'
+      minimizedTitle='Hi'
+      className='events_list'
+      close={this.close}>
       <h1>Hi</h1>
     </Panel>;
   }
