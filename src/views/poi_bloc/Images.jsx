afbbf36563b6172f644d826c0049447a9bf73be9
Guillaume Gomez <guillaume1.gomez@gmail.com>
2019-11-07 11:19:59 +0100

    Greatly improve pictures block style

--- src/views/poi_bloc/Images.jsx
+++ src/views/poi_bloc/Images.jsx
@@ -2,54 +2,33 @@
 import React from 'react';
 import { toCssUrl } from 'src/libs/url_utils';
 
-const Image = ({
-  href,
-  url,
-  classes,
-}) => <a
-  target='_blank'
-  rel='noopener noreferrer'
-  href={ href }
-  className={ classes }
-  style={{ backgroundImage: toCssUrl(url) }}
-/>;
-
-function createImages(images) {
-  if (images.length < 2) {
-    return null;
-  } else if (images.length === 2) {
-    return <div className='poi_panel__pictures_block poi_panel__pictures_second_block'>
-      <Image href={images[1].source_url} url={images[1].url} classes='poi_panel__pictures_block_full' />
-    </div>;
-  }
-  return <div
-    className={ 'poi_panel__pictures_block' +
-      ' poi_panel__pictures_second_block' +
-      ' poi_panel__pictures_block-multiple' }>
-    <Image href={images[1].source_url} url={images[1].url} classes='poi_panel__pictures_block_short' />
-    <Image href={images[2].source_url} url={images[2].url} classes='poi_panel__pictures_block_short' />
-  </div>;
-}
-
 const ImagesBlock = ({
   block,
   poi,
 }) => {
-  const images = block.images.filter(img => img.url !== poi.topImageUrl);
+  const images = block.images.filter(img => img.url !== poi.topImageUrl).slice(0, 3);
   if (!images || images.length === 0) {
     return null;
   }
 
-  return <div className="poi_panel__pictures">
-    <h5 className="poi_panel__pictures_title">
-      <i className="icon-icon_camera"></i>
+  return <div className='poi_panel__pictures'>
+    <h5 className='poi_panel__pictures_title'>
+      <i className='icon-icon_camera'></i>
       { _('Photos') }
     </h5>
-    <Image
-      href={images[0].source_url}
-      url={images[0].url}
-      classes='poi_panel__pictures_block poi_panel__pictures_first_block' />
-    { createImages(images) }
+    <div className='poi_panel__pictures_tiles'>
+      {
+        images.map(i =>
+          <a
+            key={i.url}
+            target='_blank'
+            rel='noopener noreferrer'
+            href={ i.source_url }
+            className='poi_panel__pictures_tile'
+            style={{ backgroundImage: toCssUrl(i.url) }}
+          />)
+      }
+    </div>
   </div>;
 };
 
