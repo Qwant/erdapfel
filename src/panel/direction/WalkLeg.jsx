66b7eec6b7a1b78927fbb20be65dbb7d830b43da
bbecquet <benjamin.becquet@gmail.com>
2019-11-08 12:27:15 +0100

    Public transport: Add substeps for walk legs

--- src/panel/direction/WalkLeg.jsx
+++ src/panel/direction/WalkLeg.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState, Fragment } from 'react';
 import { formatDistance } from 'src/libs/route_utils';
+import RoadMapStep from './RoadMapStep';
 
 const WalkLeg = ({ leg }) => {
+  const [ detailsOpen, setDetailsOpen ] = useState(false);
+  // @TODO: build and translate a complete summary
   const summary = `Walk on ${formatDistance(leg.distance)}`;
 
-  return <div className="itinerary_roadmap_step">
-    <div className="itinerary_roadmap_icon" />
-    <div className="itinerary_roadmap_instruction">{summary}</div>
-    <div className="itinerary_roadmap_distance" />
-  </div>;
+  return <Fragment>
+    <div className="itinerary_roadmap_step">
+      <div className="itinerary_roadmap_icon" />
+      <div className="itinerary_roadmap_instruction" onClick={() => setDetailsOpen(!detailsOpen)}>
+        {summary} <span className={`icon-icon_chevron-${detailsOpen ? 'up' : 'down'}`} />
+      </div>
+      <div className="itinerary_roadmap_distance" />
+    </div>
+    {detailsOpen && leg.steps.map((step, index) => <RoadMapStep key={index} step={step} />)}
+  </Fragment>;
 };
 
 export default WalkLeg;
