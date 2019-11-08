a47bb4f2e49fcb402727e121eb7634f66c84d7e7
bbecquet <benjamin.becquet@gmail.com>
2019-11-08 12:47:05 +0100

    Public transports: Add substeps for PT legs

--- src/panel/direction/TransportLineLeg.jsx
+++ src/panel/direction/TransportLineLeg.jsx
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { useState, Fragment } from 'react';
 import PublicTransportLine from './PublicTransportLine';
 
 const TransportLineLeg = ({ leg }) => {
+  const [ detailsOpen, setDetailsOpen ] = useState(false);
   const { mode, info = {}, stops = [] } = leg;
   const from = stops[0];
   const to = stops[stops.length - 1];
+  const middleStops = stops.slice(1, stops.length - 1);
 
-  return <div className="itinerary_roadmap_step">
-    <div className="itinerary_roadmap_icon" />
-    <div className="itinerary_roadmap_instruction">
-      <PublicTransportLine mode={mode} info={info} />
-      {from && to && <div className="itinerary_roadmap_fromTo">
-        {`${from.name} => ${to.name}`}
-      </div>}
+  return <Fragment>
+    <div className="itinerary_roadmap_step">
+      <div className="itinerary_roadmap_icon" />
+      <div className="itinerary_roadmap_instruction" onClick={() => setDetailsOpen(!detailsOpen)}>
+        <PublicTransportLine mode={mode} info={info} />
+        {from.name && to.name && <div className="itinerary_roadmap_fromTo">
+          {`${from.name} => ${to.name}`}
+        </div>}
+        {middleStops.length > 0 &&
+          <span className={`icon-icon_chevron-${detailsOpen ? 'up' : 'down'}`} />}
+      </div>
+      <div className="itinerary_roadmap_distance" />
     </div>
-    <div className="itinerary_roadmap_distance" />
-  </div>;
+    {detailsOpen && middleStops.map((stop, index) =>
+      <div key={index} className="itinerary_roadmap_step">
+        <div className="itinerary_roadmap_icon" />
+        <div className="itinerary_roadmap_instruction">{stop.name}</div>
+        <div className="itinerary_roadmap_distance" />
+      </div>)}
+  </Fragment>;
 };
 
 export default TransportLineLeg;
