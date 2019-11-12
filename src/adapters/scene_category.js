03e838b20af4e7d385d45eebbf4974d3b736a04c
Benjamin Becquet <b.becquet@qwant.com>
2019-11-12 16:01:58 +0100

    Really finish the category panel migration to React (#451)
    Category panel: remove the non-React layer

--- src/adapters/scene_category.js
+++ src/adapters/scene_category.js
@@ -1,22 +1,57 @@
 import { Marker } from 'mapbox-gl--ENV';
 import constants from '../../config/constants.yml';
 import { createIcon } from '../adapters/icon_manager';
+import Telemetry from 'src/libs/telemetry';
+import layouts from 'src/panel/layouts.js';
 
 export default class SceneCategory {
   constructor(map) {
     this.map = map;
     this.markers = [];
 
-    listen('add_category_markers', pois => {
+    listen('add_category_markers', (pois, categoryName) => {
       this.resetMarkers();
-      this.addCategoryMarkers(pois);
+      this.addCategoryMarkers(pois, categoryName);
     });
     listen('remove_category_markers', () => {
       this.removeCategoryMarkers();
     });
+    listen('highlight_category_marker', (poi, highlight) => {
+      this.highlightPoiMarker(poi, highlight);
+    });
+    listen('click_category_poi', (poi, categoryName) => {
+      this.selectPoi(poi, categoryName);
+    });
   }
 
-  addCategoryMarkers(pois) {
+  selectPoi = (poi, categoryName) => {
+    const previousMarker = document.querySelector('.mapboxgl-marker.active');
+    if (previousMarker) {
+      previousMarker.classList.remove('active');
+    }
+    if (poi.meta && poi.meta.source) {
+      Telemetry.add('open', 'poi', poi.meta.source,
+        Telemetry.buildInteractionData({
+          id: poi.id,
+          source: poi.meta.source,
+          template: 'multiple',
+          zone: 'list',
+          element: 'item',
+          category: categoryName,
+        })
+      );
+    }
+    window.app.navigateTo(`/place/${poi.toUrl()}`, {
+      poi: poi.serialize(),
+      isFromCategory: true,
+      sourceCategory: categoryName,
+      layout: layouts.LIST,
+      centerMap: true,
+    });
+    this.highlightPoiMarker(poi, true);
+  }
+
+  addCategoryMarkers(pois, categoryName) {
     this.setOsmPoisVisibility(false);
     if (pois) {
       pois.forEach(poi => {
@@ -25,7 +60,7 @@ export default class SceneCategory {
         poi.marker_id = `marker_${id}`;
         marker.onclick = function(e) {
           e.stopPropagation();
-          fire('click_category_poi', poi);
+          fire('click_category_poi', poi, categoryName);
         };
         marker.onmouseover = function(e) {
           fire('open_popup', poi, e);
@@ -58,5 +93,16 @@ export default class SceneCategory {
     constants.map.pois_layers.map(poi => {
       this.map.setLayoutProperty(poi, 'visibility', displayed ? 'visible' : 'none');
     });
+  }
+
+  highlightPoiMarker = (poi, highlight) => {
+    const marker = document.getElementById(poi.marker_id);
+    if (marker) {
+      if (highlight) {
+        marker.classList.add('active');
+      } else {
+        marker.classList.remove('active');
+      }
+    }
   }
 }
--- src/panel/category/CategoryPanel.jsx
+++ src/panel/category/CategoryPanel.jsx
@@ -6,17 +6,37 @@ import PoiCategoryItemList from './PoiCategoryItemList';
 import CategoryPanelError from './CategoryPanelError';
 import CategoryPanelHeader from './CategoryPanelHeader';
 import Telemetry from 'src/libs/telemetry';
+import SearchInput from 'src/ui_components/search_input';
+import nconf from '@qwant/nconf-getter';
+import IdunnPoi from 'src/adapters/poi/idunn_poi';
+import CategoryService from 'src/adapters/category_service';
+
+const categoryConfig = nconf.get().category;
+const MAX_PLACES = Number(categoryConfig.maxPlaces);
 
 export default class CategoryPanel extends React.Component {
   static propTypes = {
-    categoryName: PropTypes.string.isRequired,
-    pois: PropTypes.array.isRequired,
-    dataSource: PropTypes.string.isRequired,
-    hasError: PropTypes.bool,
-    zoomIn: PropTypes.bool,
-    close: PropTypes.func.isRequired,
-    selectPoi: PropTypes.func.isRequired,
-    highlightPoiMarker: PropTypes.func.isRequired,
+    categoryName: PropTypes.string,
+    query: PropTypes.string,
+    bbox: PropTypes.string,
+  }
+
+  state = {
+    pois: [],
+    dataSource: '',
+    initialLoading: true,
+  }
+
+  componentDidMount() {
+    this.mapMoveHandler = listen('map_moveend', this.fetchData);
+
+    if (this.props.categoryName) {
+      Telemetry.add(Telemetry.POI_CATEGORY_OPEN, null, null, { category: this.props.categoryName });
+      const { label } = CategoryService.getCategoryByName(this.props.categoryName);
+      SearchInput.setInputValue(label.charAt(0).toUpperCase() + label.slice(1));
+    }
+
+    this.fitMapAndFetch();
   }
 
   componentDidUpdate() {
@@ -25,6 +45,65 @@ export default class CategoryPanel extends React.Component {
       panelContent.scrollTop = 0;
     }
   }
+
+  componentWillUnmount() {
+    window.unListen(this.mapMoveHandler);
+  }
+
+  fitMapAndFetch() {
+    const rawBbox = (this.props.bbox || '').split(',');
+    const bbox = rawBbox.length === 4 && [[rawBbox[0], rawBbox[1]], [rawBbox[2], rawBbox[3]]];
+    if (bbox) {
+      window.execOnMapLoaded(() => {
+        window.map.mb.fitBounds(bbox, { animate: false });
+      });
+    }
+
+    if (window.map.mb.isMoving()) {
+      /*
+        Do not trigger API search and zoom change when the map
+        is already moving, to avoid flickering.
+        The search will be triggered on moveend.
+      */
+      return;
+    }
+
+    // Apply correct zoom when opening a category
+    const currentZoom = window.map.mb.getZoom();
+
+    // Zoom < 5: focus on Paris
+    if (currentZoom < 5) {
+      window.map.mb.flyTo({ center: [2.35, 48.85], zoom: 12 });
+    } else if (currentZoom < 12) { // Zoom < 12: zoom up to zoom 12
+      window.map.mb.flyTo({ zoom: 12 });
+    } else if (currentZoom > 16) { // Zoom > 16: dezoom to zoom 16
+      window.map.mb.flyTo({ zoom: 16 });
+    } else {
+      this.fetchData();
+    }
+  }
+
+  fetchData = async () => {
+    const bbox = window.map.mb.getBounds();
+    const urlBBox = [bbox.getWest(), bbox.getSouth(), bbox.getEast(), bbox.getNorth()]
+      .map(cardinal => cardinal.toFixed(7))
+      .join(',');
+
+    const { places, source } = await IdunnPoi.poiCategoryLoad(
+      urlBBox,
+      MAX_PLACES,
+      this.props.categoryName,
+      this.props.query
+    );
+    this.setState({
+      pois: places,
+      dataSource: source,
+      initialLoading: false,
+    });
+
+    fire('add_category_markers', places, this.props.categoryName);
+    fire('save_location');
+  };
 
   onShowPhoneNumber = poi => {
     if (poi.meta && poi.meta.source) {
@@ -41,8 +120,29 @@ export default class CategoryPanel extends React.Component {
     }
   }
 
+  close = () => {
+    SearchInput.setInputValue('');
+    window.app.navigateTo('/');
+  }
+
+  selectPoi = poi => {
+    fire('click_category_poi', poi, this.props.categoryName);
+  }
+
+  highlightPoiMarker = (poi, highlight) => {
+    fire('highlight_category_marker', poi, highlight);
+  }
+
   render() {
-    const { pois, dataSource, hasError, zoomIn, close, selectPoi, highlightPoiMarker } = this.props;
+    const { initialLoading, pois, dataSource } = this.state;
+
+    if (initialLoading) {
+      return null;
+    }
+
+    const hasError = !pois || pois.length === 0;
+    const zoomIn = !pois;
+
     let panelContent;
 
     if (hasError) {
@@ -50,8 +150,8 @@ export default class CategoryPanel extends React.Component {
     } else {
       panelContent = <PoiCategoryItemList
         pois={pois}
-        selectPoi={selectPoi}
-        highlightMarker={highlightPoiMarker}
+        selectPoi={this.selectPoi}
+        highlightMarker={this.highlightPoiMarker}
         onShowPhoneNumber={this.onShowPhoneNumber}
       />;
     }
@@ -60,7 +160,7 @@ export default class CategoryPanel extends React.Component {
       resizable
       title={<CategoryPanelHeader dataSource={dataSource} />}
       minimizedTitle={_('Show results', 'categories')}
-      close={close}
+      close={this.close}
       className="category__panel"
     >
       {panelContent}
