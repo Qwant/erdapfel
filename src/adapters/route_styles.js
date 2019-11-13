f2d79be0a02de37434462ecec80310c9cdb9464b
bbecquet <benjamin.becquet@gmail.com>
2019-11-13 14:39:10 +0100

    Routes: add line outlines to path for legibility

--- src/adapters/route_styles.js
+++ src/adapters/route_styles.js
@@ -1,11 +1,35 @@
+import Color from 'color';
+
+const darkenColor = hex => Color(hex).mix(Color('black'), 0.2).hex();
+
 const INACTIVE_ROUTE_COLOR = '#c8cbd3';
 const ACTIVE_ROUTE_COLOR = '#4ba2ea';
 const DYNAMIC_COLOR_EXPRESSION = ['case', ['has', 'lineColor'],
   ['concat', '#', ['get', 'lineColor']],
   ACTIVE_ROUTE_COLOR,
 ];
+const INACTIVE_ROUTE_COLOR_OUTLINE = darkenColor(INACTIVE_ROUTE_COLOR);
+const ACTIVE_ROUTE_COLOR_OUTLINE = darkenColor(ACTIVE_ROUTE_COLOR);
 
-export function getRouteStyle(vehicle, isActive) {
+export function getOutlineFeature(feature) {
+  const properties = { ...feature.properties };
+  properties.lineColor = properties.lineColor
+    ? darkenColor('#' + properties.lineColor).substring(1)
+    : ACTIVE_ROUTE_COLOR_OUTLINE.substring(1);
+  return {
+    ...feature,
+    properties,
+  };
+}
+
+function getColorExpression(isActive, isOutline) {
+  if (isActive) {
+    return DYNAMIC_COLOR_EXPRESSION;
+  }
+  return isOutline ? INACTIVE_ROUTE_COLOR_OUTLINE : INACTIVE_ROUTE_COLOR;
+}
+
+export function getRouteStyle(vehicle, isActive, isOutline) {
   if (vehicle === 'walking') {
     return {
       type: 'symbol',
@@ -28,19 +52,18 @@ export function getRouteStyle(vehicle, isActive) {
       'visibility': 'visible',
     },
     paint: {
-      'line-color': isActive ? DYNAMIC_COLOR_EXPRESSION : INACTIVE_ROUTE_COLOR,
+      'line-color': getColorExpression(isActive, isOutline),
       'line-color-transition': { 'duration': 0 },
-      'line-width': 7,
+      'line-width': isOutline ? 7 : 5,
     },
   };
 }
 
-export function setActiveRouteStyle(map, layerId, vehicle, isActive) {
+export function setActiveRouteStyle(map, layerId, vehicle, isActive, isOutline) {
   if (vehicle === 'walking') {
     map.setLayoutProperty(layerId, 'icon-image',
       isActive ? 'walking_bullet_active' : 'walking_bullet_inactive');
   } else {
-    map.setPaintProperty(layerId, 'line-color',
-      isActive ? DYNAMIC_COLOR_EXPRESSION : INACTIVE_ROUTE_COLOR);
+    map.setPaintProperty(layerId, 'line-color', getColorExpression(isActive, isOutline));
   }
 }
