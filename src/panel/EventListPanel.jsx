3a59fbadf0e528bb5b2375e4e0f8c03556064cfd
xem <maxime.euziere@gmail.com>
2019-11-18 15:26:15 +0100

    QMAPS-1046 wip

--- src/panel/EventListPanel.jsx
+++ src/panel/EventListPanel.jsx
@@ -5,6 +5,10 @@ import PropTypes from 'prop-types';
 import nconf from '@qwant/nconf-getter';
 import Telemetry from "../libs/telemetry";
 import events from '../../config/events.yml';
+import IdunnPoi from "../adapters/poi/idunn_poi";
+
+const eventConfig = nconf.get().events;
+const MAX_PLACES = Number(eventConfig.maxPlaces);
 
 class EventListPanel extends React.Component {
 
@@ -15,6 +19,32 @@ class EventListPanel extends React.Component {
   close = () => {
     window.app.navigateTo('/');
   }
+
+
+  /*async search() {
+    this.loading = true;
+    const bbox = window.map.mb.getBounds();
+    const urlBBox = [bbox.getWest(), bbox.getSouth(), bbox.getEast(), bbox.getNorth()]
+      .map(cardinal => cardinal.toFixed(7))
+      .join(',');
+
+    const { places, source } = await IdunnPoi.poiEventLoad(
+      urlBBox,
+      MAX_PLACES,
+      this.categoryName,
+      this.query
+    );
+    this.pois = places;
+    this.dataSource = source;
+    this.loading = false;
+
+    this.renderPanel();
+
+    this.addCategoryMarkers();
+    fire('save_location');
+  }*/
+
+
 
   render() {
     return <Panel
@@ -29,3 +59,9 @@ class EventListPanel extends React.Component {
 }
 
 export default EventListPanel;
+
+
+/*
+Data: https://maps.dev.qwant.ninja/maps/detail/v1/events?bbox=2,48,3,49&category=exhibition
+categories: concert / show / exhibition / sport / entertainment
+ */
