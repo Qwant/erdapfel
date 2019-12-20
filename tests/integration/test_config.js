78610bfcd163c71b86c38577decf01077a7e1ad5
Adrien Matissart <a.matissart@qwantresearch.com>
2019-12-20 16:57:55 +0100

    Define test_config, used by both jest and test server

---
+++ tests/integration/test_config.js
@@ -0,0 +1,17 @@
+const configBuilder = require('@qwant/nconf-builder');
+const config = configBuilder.get_without_check();
+
+// Specific config values for tests
+config.mapStyle.baseMapUrl = '[]';
+config.mapStyle.poiMapUrl = '[]';
+config.services.idunn.url = 'http://idunn_test.test';
+config.services.geocoder.url = 'http://geocoder.test/autocomplete';
+config.direction.enabled = true;
+config.direction.service.api = 'mapbox'; // Directions fixtures use mapbox format
+config.category.enabled = true;
+config.events.enabled = true;
+config.masq.enabled = false;
+config.system.baseUrl = '/maps/';
+config.server.routerBaseUrl = '/maps/';
+
+module.exports = config;
