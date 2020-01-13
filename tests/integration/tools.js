db239a77e5d20e0743281405e9271f14dca3a0b8
bbecquet <benjamin.becquet@gmail.com>
2020-01-13 11:52:10 +0100

    Remove obsolete 'wait' util for integration tests

--- tests/integration/tools.js
+++ tests/integration/tools.js
@@ -1,14 +1,6 @@
 /* globals puppeteerArguments */
 import puppeteer from 'puppeteer';
 
-export const wait = async function wait(t = 1000) {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve(t);
-    }, t);
-  });
-};
-
 export const getText = async function(page, selector) {
   return await page.evaluate(selector => {
     return document.querySelector(selector).textContent;
