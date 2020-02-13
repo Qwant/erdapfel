d94b18a22b56692a453c6be9a96e0a15b94a6a3e
G-Ray <g.bonneville@qwant.com>
2020-02-13 18:16:52 +0100

    Revert "Generate webpack-bundle-analyzer report"
    This reverts commit 5924a8dbd6cafbfc55a8accd3c656f6b14bdfb25.

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -5,7 +5,6 @@ const yaml = require('node-yaml');
 const webpack = require('webpack');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const babelConf = require('./babel.config');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const getBuildMode = function(argv) {
   const isTestMode = process.env.TEST === 'true';
@@ -80,23 +79,6 @@ const sassChunkConfig = () => {
 
 
 const mainJsChunkConfig = buildMode => {
-  const plugins = [
-    new webpack.NormalModuleReplacementPlugin(/mapbox-gl--ENV/, function(resource) {
-      if (buildMode === 'test') {
-        resource.request = resource.request.replace('--ENV', '-js-mock');
-      } else {
-        resource.request = resource.request.replace('--ENV', '');
-      }
-    }),
-    ...buildMode === 'production'
-      ? [
-        new BundleAnalyzerPlugin({
-          analyzerMode: 'static',
-          openAnalyzer: false,
-        })]
-      : [],
-  ];
-
   return {
     entry: [path.join(__dirname, '..', 'src', 'main.js')],
     output: {
@@ -112,7 +94,15 @@ const mainJsChunkConfig = buildMode => {
       },
       extensions: ['.js', '.jsx'],
     },
-    plugins: addJsOptimizePlugins(buildMode, plugins),
+    plugins: addJsOptimizePlugins(buildMode, [
+      new webpack.NormalModuleReplacementPlugin(/mapbox-gl--ENV/, function(resource) {
+        if (buildMode === 'test') {
+          resource.request = resource.request.replace('--ENV', '-js-mock');
+        } else {
+          resource.request = resource.request.replace('--ENV', '');
+        }
+      }),
+    ]),
     module: {
       rules: [{
         test: /\.yml$/,
