d84c1d9141f107d7872d457a9b2b072624085a0b
bbecquet <benjamin.becquet@gmail.com>
2020-02-27 15:58:23 +0100

    Add loading placeholders in the panel header

--- src/panel/category/CategoryPanelHeader.jsx
+++ src/panel/category/CategoryPanelHeader.jsx
@@ -1,8 +1,16 @@
 /* global _ */
 import React from 'react';
 import { sources } from 'config/constants.yml';
+import PlaceholderText from 'src/components/ui/PlaceholderText';
 
-const CategoryPanelHeader = ({ dataSource }) => {
+const CategoryPanelHeader = ({ loading, dataSource }) => {
+  if (loading) {
+    return <div className="category__panel__pj">
+      <PlaceholderText length={10} />
+      <PlaceholderText length={7} />
+    </div>;
+  }
+
   if (dataSource === sources.pagesjaunes) {
     return <div className="category__panel__pj">
       <div className="category__panel__pj_title">
