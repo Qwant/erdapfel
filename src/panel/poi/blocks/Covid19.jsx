f3b504f1ac88cb8a3a5fc6180815fe209a7bcb4a
bbecquet <benjamin.becquet@gmail.com>
2020-03-31 18:05:47 +0200

    Add a legal tooltip to Covid block

--- src/panel/poi/blocks/Covid19.jsx
+++ src/panel/poi/blocks/Covid19.jsx
@@ -3,6 +3,7 @@ import TimeTable from './TimeTable';
 import covidStrings from './covid_strings';
 import OsmSchedule from 'src/adapters/osm_schedule';
 import OpeningHour from 'src/components/OpeningHour';
+import Tooltip from 'src/components/ui/Tooltip';
 
 // @TODO: refacto OsmSchedule so it doesn't need presentational data
 const scheduleMessages = {
@@ -78,11 +79,31 @@ const getContent = ({ status, opening_hours, note, contribute_url }) => {
   return content;
 };
 
+
+/* eslint-disable */
 const Covid19 = ({ block }) => {
+  
+
   return <div className="poi_panel__info__section covid19">
-    <h4 className="poi_panel__sub_block__title">{covidStrings.blockTitle}</h4>
+    <h4 className="poi_panel__sub_block__title">
+      {covidStrings.blockTitle}{' '}
+      <Tooltip
+        triggerElement={<i className="icon-alert-triangle" />}
+        contentClassName="covid-tooltip"
+      >
+        <p>
+          Pendant toute la période de confinement, se déplacer vers ce lieu n'est autorisé
+          qu'en possession d'une attestation de déplacement dérogatoire.
+        </p>
+        <p>
+          Plus d'informations sur{' '}
+          <a href="https://www.interieur.gouv.fr/Actualites/L-actu-du-Ministere/Attestation-de-deplacement-derogatoire-et-justificatif-de-deplacement-professionnel">interieur.gouv.fr</a>
+        </p>
+      </Tooltip>  
+    </h4>
     {getContent(block)}
   </div>;
 };
+/* eslint-enable */
 
 export default Covid19;
