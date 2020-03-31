e42b3be0090b3fd02cbb89c6c1edb18579061f04
bbecquet <benjamin.becquet@gmail.com>
2020-03-31 15:16:46 +0200

    Re-use OpeningHour component everywhere possible

--- src/panel/poi/blocks/Hour.jsx
+++ src/panel/poi/blocks/Hour.jsx
@@ -1,25 +1,9 @@
-/* global _ */
 import React from 'react';
 import OsmSchedule from 'src/adapters/osm_schedule';
 import TimeTable from './TimeTable';
 import PropTypes from 'prop-types';
 import covidStrings from './covid_strings';
-
-function renderTitle(opening, covid19) {
-  if (covid19) {
-    return <span>{covidStrings.seeNormalHours}</span>;
-  }
-
-  let text = `${_(opening.status.msg)} `;
-  if (opening.nextTransition) {
-    text += ' - ' +
-      _('until {nextTransitionTime}', 'hour panel',
-        { nextTransitionTime: opening.nextTransition }) + ' ';
-  }
-  return <span>{ text }
-    <div className="poi_panel__info__hour__circle" style={{ background: opening.status.color }} />
-  </span>;
-}
+import OpeningHour from 'src/components/OpeningHour';
 
 export default class HourBlock extends React.Component {
   static propTypes = {
@@ -28,16 +12,20 @@ export default class HourBlock extends React.Component {
   }
 
   render() {
-    const opening = new OsmSchedule(this.props.block);
-    if (!opening.days) {
+    const schedule = new OsmSchedule(this.props.block);
+    if (!schedule.days) {
       return null;
     }
+
+    const timeTableTitle = this.props.covid19enabled
+      ? <span>{covidStrings.seeNormalHours}</span>
+      : <OpeningHour schedule={schedule} />;
 
     return <div className="poi_panel__info__section poi_panel__info__section--hours">
       <div className="poi_panel__info__section__description">
         <div className="icon-icon_clock poi_panel__block__symbol"></div>
         <div className="poi_panel__block__content">
-          <TimeTable title={renderTitle(opening, this.props.covid19enabled)} schedule={opening} />
+          <TimeTable schedule={schedule} title={timeTableTitle} />
         </div>
       </div>
     </div>;
--- src/panel/poi/blocks/TimeTable.jsx
+++ src/panel/poi/blocks/TimeTable.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
+import OpeningHour from 'src/components/OpeningHour';
 
 function showHour(day) {
   if (day.opening && day.opening.length > 0) {
@@ -14,29 +15,20 @@ function showHour(day) {
 function showHours(displayHours) {
   const dayNumber = new Date().getDay();
 
-  return <tbody>
-    {displayHours.map((day, i) =>
-      <tr key={i} className={
-        classnames({ 'currentDay': (i + 1) % 7 === dayNumber })
-      }>
-        <td className="day">{ day.dayName }</td>
-        <td className="hours">{ showHour(day) }</td>
-      </tr>)}
-  </tbody>;
+  return <table>
+    <tbody>
+      {displayHours.map((day, i) =>
+        <tr key={i} className={
+          classnames({ 'currentDay': (i + 1) % 7 === dayNumber })
+        }>
+          <td className="day">{ day.dayName }</td>
+          <td className="hours">{ showHour(day) }</td>
+        </tr>)}
+    </tbody>
+  </table>;
 }
 
 const TimeTable = ({ title, schedule }) => {
-  // TODO: use OpeningHour instead (careful! OsmSchedule initialization happens there as well!)
-  if (schedule.isTwentyFourSeven) {
-    return <div
-      className="timetable timetable-status poi_panel__info__hours__24_7">
-      { _('Open 24/7', 'hour block') }
-      <div className="poi_panel__info__hour__circle"
-        style={{ background: schedule.status.color }}
-      />
-    </div>;
-  }
-
   const [ isCollapsed, setCollapsed ] = useState(true);
 
   return <div className={classnames('timetable', { 'timetable--collapsed': isCollapsed })}>
@@ -45,9 +37,9 @@ const TimeTable = ({ title, schedule }) => {
       <i className="icon-icon_chevron-down" />
     </div>
     <div className={classnames('timetable-table')}>
-      <table>
-        { showHours(schedule.displayHours) }
-      </table>
+      {schedule.isTwentyFourSeven
+        ? <OpeningHour schedule={schedule} />
+        : showHours(schedule.displayHours)}
     </div>
   </div>;
 };
