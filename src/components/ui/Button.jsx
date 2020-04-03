72807224f15d8c13c18fe2dddedeeaeb5e94c8e8
bbecquet <benjamin.becquet@gmail.com>
2020-04-03 15:53:33 +0200

    Allow Button component to be used for links

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import classnames from 'classnames';
 
-const Button = ({ children, icon, variant, className = '', type = 'button', onClick, ...rest }) =>
-  <button
+const Button = ({
+  children, icon, variant, className = '', type = 'button',
+  href, onClick, ...rest
+}) => {
+  const Tag = href ? 'a' : 'button';
+
+  return <Tag
     type={type}
     className={classnames('button', { [`button--${variant}`]: variant }, className)}
     onClick={onClick}
+    href={href}
     {...rest}
   >
     {icon && <span className={`button-icon icon-${icon}`} />}
     {children && <div className="button-content">{children}</div>}
-  </button>;
+  </Tag>;
+};
 
 export default Button;
