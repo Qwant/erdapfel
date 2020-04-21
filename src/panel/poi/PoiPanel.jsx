9b9313a9fb69805f5d6132b9563e6c38def0edc5
G-Ray <g.bonneville@qwant.com>
2020-04-21 10:24:04 +0200

    PoiPanel willUnmount: set direction btn visibility

--- src/panel/poi/PoiPanel.jsx
+++ src/panel/poi/PoiPanel.jsx
@@ -87,6 +87,7 @@ export default class PoiPanel extends React.Component {
     window.unListen(this.storeRemoveHandler);
     fire('move_mobile_bottom_ui', 0);
     fire('clean_marker');
+    fire('mobile_direction_button_visibility', true);
     SearchInput.setInputValue('');
   }
 
