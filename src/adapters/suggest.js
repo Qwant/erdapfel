b95e68a59580443f438e42ecdb20d13e7d83a51d
G-Ray <g.bonneville@qwant.com>
2020-04-29 17:54:34 +0200

    Refactor suggest

--- src/adapters/suggest.js
+++ src/adapters/suggest.js
@@ -1,96 +1,115 @@
 /* global _ */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Autocomplete from '../vendors/autocomplete';
 import nconf from '@qwant/nconf-getter';
-import PoiStore from './poi/poi_store';
 import NavigatorGeolocalisationPoi from 'src/adapters/poi/specials/navigator_geolocalisation_poi';
+
+import PoiStore from './poi/poi_store';
+import Autocomplete from '../vendors/autocomplete';
+import { suggestResults } from './suggest_sources';
+import SuggestsDropdown from '../components/ui/SuggestsDropdown';
 
 const geocoderConfig = nconf.get().services.geocoder;
 const SUGGEST_MAX_ITEMS = geocoderConfig.maxItems;
 const SUGGEST_USE_FOCUS = geocoderConfig.useFocus;
 const SUGGEST_FOCUS_MIN_ZOOM = 11;
 
-import { suggestResults } from './suggest_sources';
-import SuggestsDropdown from '../components/ui/SuggestsDropdown';
 
 export default class Suggest {
-  constructor({ tagSelector, onSelect,
-    withGeoloc = false, withCategories = false,
+
+  constructor({
+    tagSelector,
+    onSelect,
+    withGeoloc = false,
+    withCategories = false,
   }) {
-    this.searchInputDomHandler = document.querySelector(tagSelector);
-    this.poi = null;
-    this.suggestList = [];
+
+    this.tagSelector = tagSelector;
     this.onSelect = onSelect;
+    this.withGeoloc = withGeoloc;
+    this.withCategories = withCategories;
+
+    this.searchInputDomHandler = document.querySelector(tagSelector);
 
     this.autocomplete = new Autocomplete({
       selector: tagSelector,
       minChars: 0,
       delay: 100,
-      source: term => suggestResults(term, {
-        withCategories,
-        useFocus: SUGGEST_USE_FOCUS,
-        focusMinZoom: SUGGEST_FOCUS_MIN_ZOOM,
-        maxFavorites: !term ? 5 : 2,
-        maxItems: SUGGEST_MAX_ITEMS,
-      }),
-      renderItems: items => {
-        const firstFav = items.findIndex(item => item instanceof PoiStore);
-        if (firstFav !== -1) {
-          items.splice(firstFav, 0, { simpleLabel: _('Favorites', 'autocomplete').toUpperCase() });
-        }
-        if (withGeoloc) {
-          items.splice(0, 0, NavigatorGeolocalisationPoi.getInstance());
-        }
-
-        // Create a react node, or reuse the existing node
-        const existingElem = document.getElementById('react-suggests-' + tagSelector);
-        let elem = null;
-        if (!existingElem) {
-          elem = document.createElement('div');
-          elem.setAttribute('id', 'react-suggests-' + tagSelector);
-          this.searchInputDomHandler.parentNode.appendChild(elem);
-        }
-
-        const reactElem = existingElem || elem;
-        const typedValue = this.searchInputDomHandler.value;
-
-        ReactDOM.render(
-          <SuggestsDropdown
-            inputId={this.searchInputDomHandler.getAttribute('id')}
-            suggestItems={items}
-            onHighlight={item => {
-              this.searchInputDomHandler.value = item && item.name || typedValue;
-            }}
-            onSelect={item => {
-              this.searchInputDomHandler.value = item.name || '';
-              this.searchInputDomHandler.blur();
-              this.onSelect(item);
-            }}
-          />
-          , reactElem
-        );
-      },
+      source: term => this.suggestResults(term),
+      renderItems: items => this.renderItems(items),
     });
 
-    this.searchInputDomHandler.addEventListener('blur', function handleSearchInputBlur() {
-      unmountReactSuggestDropdown();
+    this.searchInputDomHandler.addEventListener('blur', () => {
+      this.unmountReactSuggestDropdown();
     });
-
-    const unmountReactSuggestDropdown = () => {
-      const existingElem = document.getElementById('react-suggests-' + tagSelector);
-      if (existingElem) {
-        ReactDOM.unmountComponentAtNode(existingElem);
-        this.searchInputDomHandler.parentNode.removeChild(existingElem);
-      }
-    };
 
     this.searchInputDomHandler.onkeydown = event => {
       if (event.keyCode === 27) { // esc
-        unmountReactSuggestDropdown();
+        this.unmountReactSuggestDropdown();
       }
     };
   }
+
+  suggestResults = term =>
+    suggestResults(term, {
+      withCategories: this.withCategories,
+      useFocus: SUGGEST_USE_FOCUS,
+      focusMinZoom: SUGGEST_FOCUS_MIN_ZOOM,
+      maxItems: SUGGEST_MAX_ITEMS,
+      maxFavorites: !term ? 5 : 2,
+    });
+
+  renderItems = items => {
+    const firstFav = items.findIndex(item => item instanceof PoiStore);
+
+    if (firstFav !== -1) {
+      items.splice(firstFav, 0, { simpleLabel: _('Favorites', 'autocomplete').toUpperCase() });
+    }
+
+    if (this.withGeoloc) {
+      items.splice(0, 0, NavigatorGeolocalisationPoi.getInstance());
+    }
+
+    this.mountReactSuggestDropdown(items);
+  }
+
+  mountReactSuggestDropdown = items => {
+    // Create a react node, or reuse the existing node
+    const existingElem = document.getElementById('react-suggests-' + this.tagSelector);
+    let elem = null;
+    if (!existingElem) {
+      elem = document.createElement('div');
+      elem.setAttribute('id', 'react-suggests-' + this.tagSelector);
+      this.searchInputDomHandler.parentNode.appendChild(elem);
+    }
+
+    const reactElem = existingElem || elem;
+    const typedValue = this.searchInputDomHandler.value;
+
+    ReactDOM.render(
+      <SuggestsDropdown
+        inputId={this.searchInputDomHandler.getAttribute('id')}
+        suggestItems={items}
+        onHighlight={item => {
+          this.searchInputDomHandler.value = item && item.name || typedValue;
+        }}
+        onSelect={item => {
+          this.searchInputDomHandler.value = item.name || '';
+          this.searchInputDomHandler.blur();
+          this.onSelect(item);
+        }}
+      />
+      , reactElem
+    );
+  }
+
+  unmountReactSuggestDropdown = () => {
+    const existingElem = document.getElementById('react-suggests-' + this.tagSelector);
+    if (existingElem) {
+      ReactDOM.unmountComponentAtNode(existingElem);
+      this.searchInputDomHandler.parentNode.removeChild(existingElem);
+    }
+  };
 
   async preselect(term) {
     const suggestList = await this.autocomplete.prefetch(term);
@@ -117,10 +136,6 @@ export default class Suggest {
 
   preRender(items) {
     this.autocomplete.preRender(items);
-  }
-
-  getValue() {
-    return this.autocomplete.getValue();
   }
 
   setValue(value) {
