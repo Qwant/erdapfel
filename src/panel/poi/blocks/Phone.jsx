952bd1ca1c72fcb0fe840e8cf2757d1458050325
G-Ray <g.bonneville@qwant.com>
2020-05-22 12:22:32 +0200

    Functional PhoneBlock

--- src/panel/poi/blocks/Phone.jsx
+++ src/panel/poi/blocks/Phone.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class PhoneBlock extends React.Component {
-  static propTypes = {
-    block: PropTypes.object,
-  }
-
-  render() {
-    const { block } = this.props;
-
-    return <div className="poi_panel__info__section poi_panel__info__section--phone">
-      <div className="poi_panel__info__section__description">
-        <div className="icon-icon_phone poi_panel__block__symbol"></div>
-        <div className="poi_panel__block__content">
-          {block.local_format}
-        </div>
+const PhoneBlock = ({ block }) =>
+  <div className="poi_panel__info__section poi_panel__info__section--phone">
+    <div className="poi_panel__info__section__description">
+      <div className="icon-icon_phone poi_panel__block__symbol"></div>
+      <div className="poi_panel__block__content">
+        {block.local_format}
       </div>
-    </div>;
-  }
-}
+    </div>
+  </div>
+;
+
+PhoneBlock.propTypes = {
+  block: PropTypes.object,
+};
+
+export default PhoneBlock;
