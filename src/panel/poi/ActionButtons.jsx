13bfd6e24cc81d1c806caba56861f24c6a235d58
bbecquet <benjamin.becquet@gmail.com>
2020-06-04 15:22:40 +0200

    Simplify ActionButtons component

--- src/panel/poi/ActionButtons.jsx
+++ src/panel/poi/ActionButtons.jsx
@@ -1,61 +1,56 @@
 /* globals _ */
 import React from 'react';
 import PropTypes from 'prop-types';
-import ShareMenu from 'src/components/ui/ShareMenu';
+import { ShareMenu, Button } from 'src/components/ui';
 
-import Button from 'src/components/ui/Button';
+const ActionButtons = ({
+  poi,
+  isDirectionActive,
+  openDirection,
+  onClickPhoneNumber,
+  isPoiInFavorite,
+  toggleStorePoi,
+}) => {
+  return <div className="poi_panel__actions">
+    {isDirectionActive && <Button
+      className="poi_panel__action__direction"
+      variant="primary"
+      onClick={openDirection}
+      title={_('Directions', 'poi panel')}
+    >
+      { _('Directions', 'poi panel') }
+    </Button>}
 
-export default class ActionButtons extends React.Component {
-  static propTypes = {
-    poi: PropTypes.object.isRequired,
-    isDirectionActive: PropTypes.bool,
-    openDirection: PropTypes.func,
-    onClickPhoneNumber: PropTypes.func,
-    isPoiInFavorite: PropTypes.bool,
-    toggleStorePoi: PropTypes.func.isRequired,
-  }
-
-  renderPhone() {
-    const { onClickPhoneNumber, poi } = this.props;
-    return <Button
+    {poi?.blocksByType?.phone && <Button
       className="poi_panel__action__button poi_panel__action__phone"
       onClick={onClickPhoneNumber}
       icon="icon_phone"
       href={poi.blocksByType.phone.url}
       rel="noopener noreferrer external"
       title={_('Call', 'poi panel')}
-    />;
-  }
+    />}
 
-  render() {
-    const shouldRenderPhone = this.props.poi.blocksByType && this.props.poi.blocksByType.phone;
+    <Button
+      className="poi_panel__action__button poi_panel__action__favorite"
+      onClick={toggleStorePoi}
+      icon={isPoiInFavorite ? 'icon_star-filled' : 'star'}
+      title={_('Favorites', 'poi panel')}
+    />
 
-    return <div className="poi_panel__actions">
-      {this.props.isDirectionActive &&
-        <Button
-          className="poi_panel__action__direction"
-          variant="primary"
-          onClick={this.props.openDirection}
-          title={_('Directions', 'poi panel')}
-        >
-          { _('Directions', 'poi panel') }
-        </Button>
-      }
+    <Button className="poi_panel__action__button poi_panel__action__share"
+      title={_('Share', 'poi panel')}>
+      <ShareMenu url={window.location.toString()} scrollableParent=".panel-content"/>
+    </Button>
+  </div>;
+};
 
-      {shouldRenderPhone && this.renderPhone()}
+ActionButtons.propTypes = {
+  poi: PropTypes.object.isRequired,
+  isDirectionActive: PropTypes.bool,
+  openDirection: PropTypes.func,
+  onClickPhoneNumber: PropTypes.func,
+  isPoiInFavorite: PropTypes.bool,
+  toggleStorePoi: PropTypes.func.isRequired,
+};
 
-      <Button
-        className="poi_panel__action__button poi_panel__action__favorite"
-        onClick={this.props.toggleStorePoi}
-        icon={this.props.isPoiInFavorite ? 'icon_star-filled' : 'star'}
-        title={_('Favorites', 'poi panel')}
-      />
-
-      <Button className="poi_panel__action__button poi_panel__action__share"
-        title={_('Share', 'poi panel')}>
-        <ShareMenu url={window.location.toString()} scrollableParent=".panel-content"/>
-      </Button>
-
-    </div>;
-  }
-}
+export default ActionButtons;
