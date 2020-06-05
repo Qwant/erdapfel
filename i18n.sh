4c7413b1c1af58c5cfdd3c3557f12cad40ce77d0
Adrien Matissart <a.matissart@qwantresearch.com>
2020-06-05 17:56:43 +0200

    i18n: Extract translation keys from src/ instead of .js bundle and include location in .po files

--- i18n.sh
+++ i18n.sh
@@ -3,8 +3,9 @@
 set -ev
 find config "views/" -name "*.ejs"| xargs xgettext --language=PHP -k_n:1,2 --force-po -o tt.pot --from-code=UTF-8 -
 find config -name "*.yml"| xargs xgettext --language=Python -k_n:1,2 --force-po -o tt.pot --from-code=UTF-8 -j -
-find bin "public/build/javascript/" -name "*.js"| xargs xgettext -k_n:1,2 --force-po -o tt.pot --from-code=UTF-8 -j -
+find bin src -name "*.js" | xargs xgettext -k_n:1,2 --force-po -o tt.pot --from-code=UTF-8 -j -
+find src -name "*.jsx" | xargs xgettext --language=Python -k_n:1,2 --force-po -o tt.pot --from-code=UTF-8 -j -
 
-for i in language/message/*.po; do msgmerge --no-fuzzy-matching --no-location --update $i tt.pot  ;done
+for i in language/message/*.po; do msgmerge --no-fuzzy-matching -F --update $i tt.pot  ;done
 
 rm tt.pot
