bc70eb7d34368766c1d51956714d958e3ea33e3f
Benjamin Becquet <b.becquet@qwant.com>
2020-06-19 11:20:15 +0200

    Fix opening hours display (#684)
    * Fix and simplify rules for text of opening hour
    * Make 24-7 case more generic

--- src/components/OpeningHour.jsx
+++ src/components/OpeningHour.jsx
@@ -1,6 +1,7 @@
 /* global _ */
 import React from 'react';
 import classnames from 'classnames';
+import { capitalizeFirst } from 'src/libs/string';
 
 const getStatusMessage = status => {
   if (status === 'open') {
@@ -28,38 +29,32 @@ const OpeningHour = ({ schedule, showNextOpenOnly = false, className }) => {
   const { isTwentyFourSeven, status, nextTransition } = schedule;
   const { label, color } = getStatusMessage(status);
 
-  if (isTwentyFourSeven && !showNextOpenOnly) {
-    return <div className="openingHour poi_panel__info__hours__24_7">
-      {_('Open 24/7', 'hour block')}
-      {' '}
-      <div className="openingHour-circle u-ml-4" style={{ background: color }} />
-    </div>;
-  }
-
-  const Label = () => {
-    const displayedLabel = showNextOpenOnly && status === 'closed' ? '' : `${label}`;
-    return displayedLabel;
-  };
-
-  const NextTransition = () => {
-    if (!nextTransition || showNextOpenOnly && status === 'open') {
-      return null;
+  const getDescription = () => {
+    if (isTwentyFourSeven && !showNextOpenOnly) {
+      return _('Open 24/7', 'hour block');
     }
 
-    const separator = showNextOpenOnly && status === 'closed' ? '' : ' - ';
-    const options = { nextTransitionTime: nextTransition };
-    const text = status === 'closed'
-      ? `${_('reopening at {nextTransitionTime}', 'hour panel', options)}`
-      : `${_('until {nextTransitionTime}', 'hour panel', options)}`;
+    const parts = [];
 
-    return separator + text;
+    if (!nextTransition || status !== 'closed' || !showNextOpenOnly) {
+      parts.push(label);
+    }
+
+    if (nextTransition && (status === 'closed' || !showNextOpenOnly)) {
+      const options = { nextTransitionTime: nextTransition };
+      parts.push(status === 'closed'
+        ? _('reopening at {nextTransitionTime}', 'hour panel', options)
+        : _('until {nextTransitionTime}', 'hour panel', options));
+    }
+
+    return capitalizeFirst(parts.join(' - '));
   };
 
-  return <div className={classnames('openingHour', `openingHour--${status}`, className)}>
-    <span className="u-firstCap">
-      <Label />
-      <NextTransition />
-    </span>
+  return <div className={classnames('openingHour', {
+    [`openingHour--${status}`]: status,
+    'openingHour--24-7': isTwentyFourSeven,
+  }, className)}>
+    <div>{getDescription()}</div>
     <div className="openingHour-circle u-ml-4" style={{ background: color }} />
   </div>;
 };
