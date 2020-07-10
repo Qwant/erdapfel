a4308b075a586db4b0d21e987ad9470fb2bd89c9
bbecquet <benjamin.becquet@gmail.com>
2020-07-10 14:27:20 +0200

    Make ServicePanel collapssible on desktop

--- src/panel/service/ServicePanelDesktop.jsx
+++ src/panel/service/ServicePanelDesktop.jsx
@@ -1,15 +1,31 @@
 /* global _ */
-import React from 'react';
-import Panel from 'src/components/ui/Panel';
+import React, { Fragment, useState } from 'react';
+import { Panel, Button } from 'src/components/ui';
 import CategoryList from 'src/components/CategoryList';
 
 const ServicePanelDesktop = () => {
-  return <Panel className="service_panel" white>
-    <h3 className="u-text--smallTitle u-mb-12">
-      {_('Search around this place', 'service panel')}
-    </h3>
-    <CategoryList className="service_panel__categories" />
-  </Panel>;
+  const [ collapsed, setCollapsed ] = useState(true);
+
+  return <Fragment>
+    <Panel className="service_panel u-mb-8" white>
+      <h3 className="u-text--smallTitle u-mb-12">
+        {_('Search around this place', 'service panel')}
+      </h3>
+      <CategoryList
+        className="service_panel__categories"
+        limit={collapsed ? 4 : undefined}
+      />
+    </Panel>
+    <div className="u-center">
+      <Button
+        variant="shadow"
+        icon={collapsed ? 'icon_chevron-down' : 'icon_chevron-up'}
+        onClick={() => setCollapsed(!collapsed)}
+      >
+        {collapsed ? _('Display more proximity services') : _('Reduce')}
+      </Button>
+    </div>
+  </Fragment>;
 };
 
 export default ServicePanelDesktop;
