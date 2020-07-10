7d370f6bba2c08908df7561721b89ba4e6237338
G-Ray <g.bonneville@qwant.com>
2020-07-10 11:52:52 +0200

    Fix MasqAlert dismissed in LocalStorage

--- src/components/ui/MasqAlert.jsx
+++ src/components/ui/MasqAlert.jsx
@@ -7,14 +7,14 @@ const masqAlertDate = (new Date(nconf.get().masq.alertDate)).toDateString();
 
 const MasqAlert = () => {
   const masqDismissed = window.localStorage.getItem('masq_alert_dismissed');
-  const [isVisible, setIsVisible] = useState(masqAlertEnabled && masqDismissed !== true);
+  const [isVisible, setIsVisible] = useState(masqAlertEnabled && masqDismissed !== 'true');
   if (!isVisible) {
     return null;
   }
 
   const dismiss = () => {
     setIsVisible(false);
-    window.localStorage.setItem('masq_alert_dismissed', true);
+    window.localStorage.setItem('masq_alert_dismissed', 'true');
   };
 
   return <Alert
