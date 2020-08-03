7c03f47dfc0734e819bb2351f2589bffeb274f0b
G-Ray <g.bonneville@qwant.com>
2020-08-03 18:19:40 +0200

    Set Block title as u-text--caption

--- src/components/ui/Block.jsx
+++ src/components/ui/Block.jsx
@@ -5,7 +5,7 @@ const Block = ({ icon, title, children, className }) =>
   <div className={classNames('block', className)}>
     <i className={`block-icon icon-${icon}`}/>
     <div className="block-content">
-      <div className="u-firstCap u-text--subtitle u-mb-2">{title}</div>
+      <div className="u-firstCap u-text--caption u-mb-2">{title}</div>
       {children}
     </div>
   </div>;
