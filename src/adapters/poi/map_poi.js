77c51825545e6ad0b81df6169e6941c9a6396028
bbecquet <benjamin.becquet@gmail.com>
2020-09-03 20:26:54 +0200

    Allow to use id when deserializing POI from map

--- src/adapters/poi/map_poi.js
+++ src/adapters/poi/map_poi.js
@@ -4,12 +4,12 @@ import Poi, { POI_TYPE } from './poi';
 export default class MapPoi extends Poi {
   constructor(feature) {
     const {
-      global_id: id,
+      global_id,
       ['class']: className,
       subclass: subClassName,
       name,
     } = feature.properties;
     const ll = LngLat.convert(feature.geometry.coordinates);
-    super(id, name, POI_TYPE, ll, className, subClassName);
+    super(global_id || feature.id, name, POI_TYPE, ll, className, subClassName);
   }
 }
