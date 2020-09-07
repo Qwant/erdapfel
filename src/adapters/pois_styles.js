292dd2d755c6d5517eccd89504cb87a0ff0de36a
bbecquet <benjamin.becquet@gmail.com>
2020-09-07 17:48:34 +0200

    Manage hovered and selected POI as another layer

--- src/adapters/pois_styles.js
+++ src/adapters/pois_styles.js
@@ -5,5 +5,16 @@ export const filteredPoisStyle = {
     'icon-image': 'pin_with_dot',
     'icon-allow-overlap': true,
     'icon-size': 0.5,
+    'icon-anchor': 'bottom',
+  },
+};
+
+export const hoveredPoiStyle = {
+  type: 'symbol',
+  layout: {
+    'icon-image': 'pin_with_dot',
+    'icon-allow-overlap': true,
+    'icon-size': 0.66,
+    'icon-anchor': 'bottom',
   },
 };
