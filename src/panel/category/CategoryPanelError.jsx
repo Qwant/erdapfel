5feabfa022c5e82dd983973d0f4b9aad2ccee212
G-Ray <g.bonneville@qwant.com>
2020-09-14 12:19:50 +0200

    Update category error for consistency

--- src/panel/category/CategoryPanelError.jsx
+++ src/panel/category/CategoryPanelError.jsx
@@ -1,22 +1,26 @@
 /* global _ */
 import React from 'react';
 
-const geoloc = () => {
+const geoloc = e => {
+  e.preventDefault();
   document.querySelector('.mapboxgl-ctrl-geolocate').click();
 };
 
 const CategoryPanelError = ({ zoomIn }) => {
-  return <div className="category__panel__error">
-    <p>
-      {zoomIn
-        ? _('Please zoom in the map to see the results for this category.', 'categories')
-        : _('No results found. Please zoom out of the map.', 'categories')
-      }
-    </p>
-    <button onClick={geoloc}>
-      <span className="icon-pin_geoloc" /> {_('Search around my position', 'categories')}
-    </button>
-  </div>;
+  return (
+    <div className="category__panel__error">
+      <p className="u-mb-8 u-text--smallTitle">{_('No results found.')}</p>
+      <p className="u-mb-20">
+        {zoomIn
+          ? _('Please zoom in the map to see the results for this category.', 'categories')
+          : _('Please zoom out of the map.', 'categories')
+        }
+      </p>
+      <a onClick={geoloc} href="#">
+        <span className="icon-pin_geoloc" /> {_('Search around my position', 'categories')}
+      </a>
+    </div>
+  );
 };
 
 export default CategoryPanelError;
