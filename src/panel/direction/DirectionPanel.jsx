a79deb40d2d0d47912cc5315048c93b02a2301de
G-Ray <g.bonneville@qwant.com>
2020-09-21 17:08:02 +0200

    direction: restore a close button

--- src/panel/direction/DirectionPanel.jsx
+++ src/panel/direction/DirectionPanel.jsx
@@ -312,8 +312,18 @@ export default class DirectionPanel extends React.Component {
             <MobileRoadMapPreview steps={getAllSteps(activePreviewRoute)} />}
         </Fragment>
         : <Panel
-          renderHeader={title}
-          close={this.onClose}
+          renderHeader={(
+            <div>
+              {title}
+              <div
+                className="direction-close"
+                title={_('Close')}
+                onClick={this.onClose}
+              >
+                <i className="icon-x" />
+              </div>
+            </div>
+          )}
           className="direction_panel"
         >
           {form}
