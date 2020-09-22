16f3769a2d23c6564a2a41b7e10bd349486b9fb1
bbecquet <benjamin.becquet@gmail.com>
2020-09-22 11:26:32 +0200

    Remove Masq elements and related parts from Menu

--- src/panel/menu/MenuButton.jsx
+++ src/panel/menu/MenuButton.jsx
@@ -1,22 +1,18 @@
 /* global _ */
 import React from 'react';
-import MasqAvatar from './MasqAvatar';
 
-const MenuButton = ({ masqUser, onClick }) => {
-  if (!masqUser) {
-    return (
-      <div
-        className="menu__button menu__button_not_logged"
-        onClick={onClick}
-        title={_('Menu')}
-      >
-        <div className="menu__button__line" />
-        <div className="menu__button__line" />
-        <div className="menu__button__line" />
-      </div>
-    );
-  }
-  return <MasqAvatar user={masqUser} className="menu__button" onClick={onClick} />;
+const MenuButton = ({ onClick }) => {
+  return (
+    <div
+      className="menu__button menu__button_not_logged"
+      onClick={onClick}
+      title={_('Menu')}
+    >
+      <div className="menu__button__line" />
+      <div className="menu__button__line" />
+      <div className="menu__button__line" />
+    </div>
+  );
 };
 
 export default MenuButton;
