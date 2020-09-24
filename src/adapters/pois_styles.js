6c98ef182f9b4efc314ef487df09f0b7dc23bdae
bbecquet <benjamin.becquet@gmail.com>
2020-09-24 12:14:16 +0200

    Add label styling to POI marker layer

--- src/adapters/pois_styles.js
+++ src/adapters/pois_styles.js
@@ -7,5 +7,20 @@ export const filteredPoisStyle = {
     'icon-ignore-placement': true,
     'icon-size': 0.5,
     'icon-anchor': 'bottom',
+
+    'text-font': [ 'Noto Sans Bold' ],
+    'text-size': 10,
+    'text-field': '{name}',
+    'text-allow-overlap': false,
+    'text-ignore-placement': false,
+    'text-optional': true,
+    'text-anchor': 'top',
+    'text-radial-offset': 0.25,
+  },
+  paint: {
+    'text-color': ['case', ['==', ['feature-state', 'selected'], true], '#900014', '#0c0c0e'],
+    'text-halo-color': 'white',
+    'text-halo-width': 1,
+    'text-translate': [0, 4],
   },
 };
