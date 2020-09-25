9fdbc18706c4ff5d2b9c3f2f2bcb56e64a636258
bbecquet <benjamin.becquet@gmail.com>
2020-09-25 13:53:37 +0200

    Adjust label positions and margins

--- src/adapters/pois_styles.js
+++ src/adapters/pois_styles.js
@@ -14,13 +14,14 @@ export const filteredPoisStyle = {
     'text-allow-overlap': false,
     'text-ignore-placement': false,
     'text-optional': true,
-    'text-anchor': 'top',
-    'text-radial-offset': 0.25,
+    'text-variable-anchor': ['top', 'bottom-left', 'bottom-right'],
+    'text-offset': [1.5, 0.5],
+    'text-justify': 'auto',
   },
   paint: {
     'text-color': ['case', ['==', ['feature-state', 'selected'], true], '#900014', '#0c0c0e'],
     'text-halo-color': 'white',
     'text-halo-width': 1,
-    'text-translate': [0, 4],
+    'text-translate': [0, -2],
   },
 };
