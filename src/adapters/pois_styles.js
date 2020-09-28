816be15755ccca29765ee34b580471db689ab7f1
Benjamin Becquet <b.becquet@qwant.com>
2020-09-28 18:26:17 +0200

    Add label styling to POI marker layer (#797)

--- src/adapters/pois_styles.js
+++ src/adapters/pois_styles.js
@@ -7,5 +7,21 @@ export const filteredPoisStyle = {
     'icon-ignore-placement': true,
     'icon-size': 0.5,
     'icon-anchor': 'bottom',
+
+    'text-font': [ 'Noto Sans Bold' ],
+    'text-size': 10,
+    'text-field': '{name}',
+    'text-allow-overlap': false,
+    'text-ignore-placement': false,
+    'text-optional': true,
+    'text-variable-anchor': ['top', 'bottom-left', 'bottom-right'],
+    'text-offset': [1.5, 0.5],
+    'text-justify': 'auto',
+  },
+  paint: {
+    'text-color': ['case', ['==', ['feature-state', 'selected'], true], '#900014', '#0c0c0e'],
+    'text-halo-color': 'white',
+    'text-halo-width': 1,
+    'text-translate': [0, -2],
   },
 };
