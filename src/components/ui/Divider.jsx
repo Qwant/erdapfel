0657002cfadb02da5406514527ba3ef1cb206909
bbecquet <benjamin.becquet@gmail.com>
2020-10-13 11:29:49 +0200

    Fix left margin of direction form divider

--- src/components/ui/Divider.jsx
+++ src/components/ui/Divider.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import cx from 'classnames';
 
-const Divider = ({ className, paddingTop = 20, paddingBottom = 20, marginLeft = 0 }) =>
+const Divider = ({ className, paddingTop = 20, paddingBottom = 20 }) =>
   <div
     className={cx('divider', className)}
-    style={{
-      padding: `${paddingTop}px 0 ${paddingBottom}px 0`,
-      marginLeft,
-    }}
+    style={{ padding: `${paddingTop}px 0 ${paddingBottom}px 0` }}
   >
     <div className="divider-line" />
   </div>
