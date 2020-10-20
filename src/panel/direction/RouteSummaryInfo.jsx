e9e99dc4fde5799fb10dd544c407b4a7b66a1bab
G-Ray <g.bonneville@qwant.com>
2020-10-20 12:06:42 +0200

    Rm CloseButton from RouteSummaryInfo

--- src/panel/direction/RouteSummaryInfo.jsx
+++ src/panel/direction/RouteSummaryInfo.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
 import RouteVia from './RouteVia';
-import { Badge, CloseButton, Flex } from 'src/components/ui';
+import { Badge, Flex } from 'src/components/ui';
 import { formatDuration, formatDistance } from 'src/libs/route_utils';
 
-const RouteSummaryInfo = ({ route, vehicle, onClose }) =>
+const RouteSummaryInfo = ({ route, vehicle }) =>
   <div>
     <Flex alignItems="center" justifyContent="space-between" className="u-text--title">
       {formatDuration(route.duration)}
-      {onClose && <CloseButton onClick={onClose} />}
     </Flex>
 
     <RouteVia className="u-mb-8" route={route} vehicle={vehicle} />
