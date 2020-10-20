72e059f5e1cf9fe1b94ce9e5a4a6d1c15058a406
G-Ray <g.bonneville@qwant.com>
2020-10-20 11:45:45 +0200

    Use RouteSummaryInfo on mobile

--- src/panel/direction/MobileRouteDetails.jsx
+++ src/panel/direction/MobileRouteDetails.jsx
@@ -1,10 +1,10 @@
 /* global _ */
 import React, { useRef } from 'react';
-import RouteVia from './RouteVia';
 import RoadMap from './RoadMap';
-import { Badge, Button, CloseButton, Divider, Flex } from 'src/components/ui';
-import { formatDuration, formatDistance } from 'src/libs/route_utils';
+import { Button, Divider, Flex } from 'src/components/ui';
 import classnames from 'classnames';
+
+import RouteSummaryInfo from './RouteSummaryInfo';
 
 const MobileRouteDetails =
 ({ id, route, origin, destination, vehicle, toggleDetails, openPreview }) => {
@@ -18,22 +18,18 @@ const MobileRouteDetails =
   >
     <div className="mobile-route-details-header">
       <div className="mobile-route-details-header-content">
-        <Flex alignItems="center" justifyContent="space-between" className="u-text--title">
-          {formatDuration(route.duration)}
-          <CloseButton onClick={() => { toggleDetails(id); }} />
-        </Flex>
-
-        <RouteVia className="u-mb-8" route={route} vehicle={vehicle} />
-        <Badge className={isPublicTransport ? 'u-mb-20' : ''}>
-          {formatDistance(route.distance)}
-        </Badge>
+        <RouteSummaryInfo
+          route={route}
+          vehicle={vehicle}
+          onClose={() => toggleDetails(id)}
+        />
 
         {isPublicTransport &&
           <Button
             className="u-firstCap"
             onClick={() => { openPreview(id); }}
             variant="primary"
-            style={{ width: '100%' }}
+            style={{ width: '100%', marginTop: 20 }}
           >
             <Flex alignItems="center" justifyContent="center">
               <img className="u-mr-4" src="./statics/images/direction_icons/guide.svg" />
