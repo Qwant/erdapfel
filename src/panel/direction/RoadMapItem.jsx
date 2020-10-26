04709ce6d42b1cb94ac14943f2cc9456f64d9ca9
Benjamin Becquet <b.becquet@qwant.com>
2020-10-26 10:39:16 +0100

    Restore the layout of public transport roadmaps (#879)

--- src/panel/direction/RoadMapItem.jsx
+++ src/panel/direction/RoadMapItem.jsx
@@ -3,19 +3,22 @@ import classnames from 'classnames';
 
 import { Divider, Flex } from 'src/components/ui';
 
-const RoadMapItem = ({ children, icon, distance, className, ...rest }) =>
-  <div className={classnames('itinerary_roadmap_item', className)} {...rest}>
-    <Flex className="itinerary_roadmap_item-block">
-      <div className={classnames(
-        'itinerary_roadmap_icon',
-        { [`itinerary_roadmap_icon_${icon}`]: icon }
-      )} />
-      <div>
-        <span className="itinerary_roadmap_instruction u-text--smallTitle">{children}</span>
-        <div className="u-text--subtitle">{distance}</div>
-      </div>
-    </Flex>
+const RoadMapItem = ({ children, icon, distance, className, line, ...rest }) =>
+  <>
+    <div className={classnames('itinerary_roadmap_item', className)} {...rest}>
+      {line}
+      <Flex alignItems="flex-start">
+        <div className={classnames(
+          'itinerary_roadmap_icon',
+          { [`itinerary_roadmap_icon_${icon}`]: icon }
+        )} />
+        <div className="itinerary_roadmap_step_description">
+          <span className="itinerary_roadmap_instruction u-text--smallTitle">{children}</span>
+          <div className="u-text--subtitle">{distance}</div>
+        </div>
+      </Flex>
+    </div>
     <Divider paddingTop={0} paddingBottom={0} />
-  </div>;
+  </>;
 
 export default RoadMapItem;
