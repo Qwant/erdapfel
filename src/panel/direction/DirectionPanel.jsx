28a6781647b891bf65f07ed467ff668303fafb1a
G-Ray <g.bonneville@qwant.com>
2020-10-29 16:27:16 +0100

    add u-mr-16 to share button

--- src/panel/direction/DirectionPanel.jsx
+++ src/panel/direction/DirectionPanel.jsx
@@ -403,7 +403,7 @@ export default class DirectionPanel extends React.Component {
           <div id="direction-autocomplete_suggestions" />
           <ShareMenu url={window.location.toString()}>
             {openMenu => <Button
-              className="u-ml-auto u-flex-shrink-0"
+              className="u-ml-auto u-flex-shrink-0 u-mr-16"
               variant="tertiary"
               title={_('Share itinerary', 'direction')}
               onClick={e => this.handleShareClick(e, openMenu)}
