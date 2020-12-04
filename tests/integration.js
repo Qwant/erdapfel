529ba38a9a29ebf5cb639ab0945279b13fee25c6
bbecquet <benjamin.becquet@gmail.com>
2020-12-04 16:00:23 +0100

    Run integration tests on two different device configurations

--- tests/integration.js
+++ tests/integration.js
@@ -1,11 +1,12 @@
 const config = require('./integration/test_config');
+const { getIgnorePatterns } = require('./integration/device');
 
 module.exports = {
   setupFilesAfterEnv: ['jest-extended'],
   testMatch: [`${__dirname}/integration/tests/*.js`],
   globalSetup: `${__dirname}/integration/server_start.js`,
   globalTeardown: `${__dirname}/integration/server_close.js`,
-  testPathIgnorePatterns: ['/node_modules/'],
+  testPathIgnorePatterns: ['/node_modules/'].concat(getIgnorePatterns(process.env.TEST_DEVICE)),
   'rootDir': __dirname + '/../',
   'verbose': true,
   'collectCoverage': false,
---
+++ tests/integration/device.js
@@ -0,0 +1,26 @@
+
+const viewports = {
+  mobile: {
+    width: 400,
+    height: 800,
+    hasTouch: true,
+  },
+};
+
+const ignorePatterns = {
+  mobile: ['.desktop.'],
+  desktop: ['.mobile.'],
+};
+
+function getPupeeterViewport(deviceName) {
+  return viewports[deviceName] || null;
+}
+
+function getIgnorePatterns(deviceName) {
+  return ignorePatterns[deviceName] || [];
+}
+
+module.exports = {
+  getPupeeterViewport,
+  getIgnorePatterns,
+};
