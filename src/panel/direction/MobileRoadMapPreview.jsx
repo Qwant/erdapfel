6458d19a8c98e4b2fe79cc411afbe9e0bf30e175
xem <maxime.euziere@gmail.com>
2020-12-08 16:56:47 +0100

    QMAPS-1772 functional conversion

--- src/panel/direction/MobileRoadMapPreview.jsx
+++ src/panel/direction/MobileRoadMapPreview.jsx
@@ -1,81 +1,98 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import RoadMapStep from './RoadMapStep';
 import { fire } from 'src/libs/customEvents';
 import classnames from 'classnames';
 import { Button } from '../../components/ui';
 
-export default class MobileRoadMapPreview extends React.Component {
-  constructor(props) {
-    super(props);
-    this.stepsRef = React.createRef();
-  }
+const MobileRoadMapPreview = ({
+  steps,
+  onClose,
+}) => {
+  const [currentStep, setCurrentStep] = useState(0);
+  const [expanded, setExpanded] = useState(false);
+  const stepsRef = React.createRef();
 
-  static propTypes = {
-    steps: PropTypes.array.isRequired,
-    onClose: PropTypes.func.isRequired,
-  };
+  const scroll = () => {
 
-  state = {
-    currentStep: 0,
-    expanded: false,
-  };
-
-  componentDidMount() {
-    fire('move_mobile_bottom_ui', 105);
-    fire('zoom_step', this.props.steps[this.state.currentStep]);
-  }
-
-  scroll = () => {
-
-    const currentStep = Math.floor(
+    const newStep = Math.floor(
       // Divide the step container's scrollLeft up to the middle of the screen with the size of a step
       // to determine which step is present at the middle of the screen
-      (this.stepsRef.current.scrollLeft + window.innerWidth / 2) / (window.innerWidth - 70 + 12)
+      (stepsRef.current.scrollLeft + window.innerWidth / 2) / (window.innerWidth - 70 + 12)
     );
 
     // If it has changed, save it and highlight it as the current step
-    if (this.state.currentStep !== currentStep) {
-      this.setState({ currentStep });
-      fire('zoom_step', this.props.steps[currentStep]);
+    if (currentStep !== newStep) {
+      setCurrentStep(newStep);
+      fire('zoom_step', steps[newStep]);
     }
   };
 
-  toggleSize = () => {
-    this.setState( { expanded: !this.state.expanded } );
-    fire('move_mobile_bottom_ui', this.stepsRef.current.offsetHeight);
+  const toggleSize = () => {
+    setExpanded(!expanded);
+    fire('move_mobile_bottom_ui', stepsRef.current.offsetHeight);
   };
 
-  render() {
-    return <div className="itinerary_mobile_step_by_step">
-      <Button
-        className="mobile-roadmap-preview-close"
-        onClick={this.props.onClose}
-        icon="arrow-left"
-      />
-      <div
-        ref={this.stepsRef}
-        className={classnames('mobile-roadmap-preview-steps', { expanded: this.state.expanded })}
-        onScroll={this.scroll}
-        onClick={this.toggleSize}
-      >
-        {
-          this.props.steps.map((step, index) =>
-            <div
-              key={index}
-              className={
-                classnames(
-                  'itinerary_mobile_step',
-                  {
-                    past: index < this.state.currentStep,
-                    active: index === this.state.currentStep,
-                  }
-                )
-              }>
-              <RoadMapStep step={step}/>
-            </div>)
-        }
-      </div>
-    </div>;
-  }
-}
+  // On load, zoom on step 0, unexpanded
+  useEffect(
+    () => {
+      fire('move_mobile_bottom_ui', 105);
+      fire('zoom_step', steps[0]);
+    },
+    []
+  );
+
+  // When expanded is modified, move the map's bottom UI
+  useEffect(
+    () => {
+      fire('move_mobile_bottom_ui', stepsRef.current.offsetHeight);
+    },
+    [expanded]
+  );
+
+  // When currentStep is modified, zoom on it
+  useEffect(
+    () => {
+      fire('zoom_step', steps[currentStep]);
+    },
+    [currentStep]
+  );
+
+  return <div className="itinerary_mobile_step_by_step">
+    <Button
+      className="mobile-roadmap-preview-close"
+      onClick={onClose}
+      icon="arrow-left"
+    />
+    <div
+      ref={stepsRef}
+      className={classnames('mobile-roadmap-preview-steps', { expanded })}
+      onScroll={scroll}
+      onClick={toggleSize}
+    >
+      {
+        steps.map((step, index) =>
+          <div
+            key={index}
+            className={
+              classnames(
+                'itinerary_mobile_step',
+                {
+                  past: index < currentStep,
+                  active: index === currentStep,
+                }
+              )
+            }>
+            <RoadMapStep step={step}/>
+          </div>)
+      }
+    </div>
+  </div>;
+};
+
+MobileRoadMapPreview.propTypes = {
+  steps: PropTypes.array.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default MobileRoadMapPreview;
