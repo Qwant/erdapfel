554bfa4dec387d4e618b0753cc409250938efeff
Adrien Matissart <a.matissart@qwantresearch.com>
2020-12-09 12:44:21 +0100

    fix transifex script

--- .tx/push.sh
+++ .tx/push.sh
@@ -6,6 +6,6 @@ npm run i18n
 # Remove msgstr from en.po so that it's considered as a .pot template file by Transifex
 msgfilter --keep-header -i language/message/en.po -o language/message/en.po true
 
-python3.5 -m pip install --user transifex-client==0.13.12
+sudo pip3 install transifex-client==0.13.12
 sudo echo $'[https://www.transifex.com]\nhostname = https://www.transifex.com\nusername = '"$TRANSIFEX_API_USER"$'\npassword = '"$TRANSIFEX_API_KEY"$'\n' > ~/.transifexrc
 tx push -s
