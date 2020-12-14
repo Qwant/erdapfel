068f26f85da3cb290250249056ca8af1ce1f4074
G-Ray <g.bonneville@qwant.com>
2020-12-14 16:48:55 +0100

    Placeicon: Accept icon and color passed as props

--- src/components/PlaceIcon.jsx
+++ src/components/PlaceIcon.jsx
@@ -3,11 +3,13 @@ import IconManager from 'src/adapters/icon_manager';
 import { getLightBackground } from 'src/libs/colors';
 import classnames from 'classnames';
 
-const PlaceIcon = ({ place, withBackground }) => {
-  const { iconClass, color, icomoon } = IconManager.get(place);
+const PlaceIcon = ({ place, withBackground, className, ...rest }) => {
+  const { iconClass, color, icomoon } = place // Place should be a poi
+    ? IconManager.get(place)
+    : rest;
 
   return <div
-    className={classnames('placeIcon', `icon-${iconClass}`, { icon: !icomoon })}
+    className={classnames('placeIcon', `icon-${iconClass}`, { icon: !icomoon }, className)}
     style={{
       color,
       backgroundColor: withBackground ? getLightBackground(color) : null,
