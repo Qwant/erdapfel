362fd04461f16b13731ff881c277b71145f7e492
G-Ray <g.bonneville@qwant.com>
2021-01-05 17:47:39 +0100

    Add direction icon

--- src/components/ui/icons.js
+++ src/components/ui/icons.js
@@ -1,2 +1,3 @@
 export { default as Heart } from '../../../public/images/feather/heart.svg';
 export { default as Magnifier } from '../../../public/images/magnifier-dark.svg';
+export { default as Direction } from '../../../public/images/direction-line-white.svg';
