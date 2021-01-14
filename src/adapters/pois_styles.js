d471c696a151aa2ca79c6f496f92bf256144c2b3
Benjamin Becquet <b.becquet@qwant.com>
2021-01-14 16:40:53 +0100

    Define a hover style for POI labels (#1008)
    * Define a hover style for POI labels

--- src/adapters/pois_styles.js
+++ src/adapters/pois_styles.js
@@ -17,8 +17,6 @@ export const getFilteredPoisPinStyle = () => ({
   },
 });
 
-
-
 export const getFilteredPoisLabelStyle = () => ({
   type: 'symbol',
   layout: {
@@ -39,3 +37,17 @@ export const getFilteredPoisLabelStyle = () => ({
     'text-translate': [0, -2],
   },
 });
+
+const COLOR_ACTION_BLUE_BASE = '#1a6aff';
+
+export const setPoiHoverStyle = (map, poiLayer) => {
+  if (!map.getPaintProperty) {
+    // @MAPBOX: This method isn't implemented by the Mapbox-GL mock
+    return;
+  }
+  const textColorProperty = map.getPaintProperty(poiLayer, 'text-color');
+  map.setPaintProperty(poiLayer, 'text-color',
+    ['case', ['to-boolean', ['feature-state', 'hover']], COLOR_ACTION_BLUE_BASE, textColorProperty],
+    { validate: false },
+  );
+};
