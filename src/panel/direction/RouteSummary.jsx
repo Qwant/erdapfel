11c2d246326f2a36294f0ea44490c66d7875dc52
bbecquet <benjamin.becquet@gmail.com>
2021-01-18 15:58:09 +0100

    Convert RouteSummary to functional component

--- src/panel/direction/RouteSummary.jsx
+++ src/panel/direction/RouteSummary.jsx
@@ -2,63 +2,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Flex } from 'src/components/ui';
-import Telemetry from 'src/libs/telemetry';
 
 import RouteSummaryInfo from './RouteSummaryInfo';
 
-export default class RouteSummary extends React.Component {
-  static propTypes = {
-    id: PropTypes.number.isRequired,
-    route: PropTypes.object.isRequired,
-    vehicle: PropTypes.string.isRequired,
-    toggleDetails: PropTypes.func.isRequired,
-    selectRoute: PropTypes.func.isRequired,
-    isActive: PropTypes.bool.isRequired,
-    showDetails: PropTypes.bool.isRequired,
-  }
-
-  onClick = () => {
-    this.props.selectRoute(this.props.id);
-  }
-
-  onClickDetails = event => {
+const RouteSummary = (
+  { id, route, vehicle, toggleDetails, selectRoute, isActive, showDetails }
+) => {
+  const onClickDetails = event => {
     event.stopPropagation();
-    this.props.toggleDetails(this.props.id);
-  }
+    toggleDetails(id);
+  };
 
-  onShareClick = (e, handler) => {
-    Telemetry.add(Telemetry.ITINERARY_SHARE);
-    return handler(e);
-  }
+  return <Flex
+    className="itinerary_leg_summary"
+    justifyContent="space-between"
+    alignItems="flex-end"
+    onClick={() => { selectRoute(id); }}
+  >
+    <RouteSummaryInfo
+      isFastest={id === 0}
+      route={route}
+      vehicle={vehicle}
+    />
 
-  render() {
-    const { id, route, isActive, showDetails, vehicle } = this.props;
+    {isActive &&
+      <Button
+        className="itinerary_leg_detailsBtn u-firstCap"
+        onClick={onClickDetails}
+        icon={showDetails ? null : 'icon_list'}
+        variant={showDetails ? 'tertiary' : 'secondary'}
+      >
+        {showDetails
+          ? _('See less', 'direction')
+          : _('Details', 'direction')
+        }
+      </Button>
+    }
+  </Flex>;
+};
 
-    return <Flex
-      className="itinerary_leg_summary"
-      justifyContent="space-between"
-      alignItems="flex-end"
-      onClick={this.onClick}
-    >
-      <RouteSummaryInfo
-        isFastest={id === 0}
-        route={route}
-        vehicle={vehicle}
-      />
+RouteSummary.propTypes = {
+  id: PropTypes.number.isRequired,
+  route: PropTypes.object.isRequired,
+  vehicle: PropTypes.string.isRequired,
+  toggleDetails: PropTypes.func.isRequired,
+  selectRoute: PropTypes.func.isRequired,
+  isActive: PropTypes.bool.isRequired,
+  showDetails: PropTypes.bool.isRequired,
+};
 
-      {isActive &&
-        <Button
-          className="itinerary_leg_detailsBtn u-firstCap"
-          onClick={this.onClickDetails}
-          icon={showDetails ? null : 'icon_list'}
-          variant={showDetails ? 'tertiary' : 'secondary'}
-        >
-          {showDetails
-            ? _('See less', 'direction')
-            : _('Details', 'direction')
-          }
-        </Button>
-      }
-    </Flex>;
-  }
-}
+export default RouteSummary;
