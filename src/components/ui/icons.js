7747f940008abf51dde328d4018afa1f726a308b
bbecquet <benjamin.becquet@gmail.com>
2021-01-26 14:39:51 +0100

    Expose new icons as React components

--- src/components/ui/icons.js
+++ src/components/ui/icons.js
@@ -1,2 +1,8 @@
 export { default as Heart } from '../../../public/images/feather/heart.svg';
 export { default as Magnifier } from '../../../public/images/magnifier-dark.svg';
+
+export { default as IconLightbulb } from '../../../public/images/remix/lightbulb-line.svg';
+export { default as IconEdit } from '../../../public/images/remix/edit-line.svg';
+export { default as IconExternalLink } from '../../../public/images/remix/external-link-line.svg';
+export { default as IconCloseCircle } from '../../../public/images/remix/close-circle-line.svg';
+export { default as IconMenu } from '../../../public/images/remix/menu-line.svg';
