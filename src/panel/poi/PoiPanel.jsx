e4ff407668946ebd16421b634d1ac8265f1a9e3b
G-Ray <g.bonneville@qwant.com>
2021-03-04 18:47:16 +0100

    fix shouldShowBackToQwant calls

--- src/panel/poi/PoiPanel.jsx
+++ src/panel/poi/PoiPanel.jsx
@@ -262,7 +262,7 @@ export default class PoiPanel extends React.Component {
             (!poiFilters || !poiFilters.category),
           })}
           renderHeader={<NavHeader isMobile={isMobile} />}
-          floatingItemsLeft={isMobile && shouldShowBackToQwant && [
+          floatingItemsLeft={isMobile && shouldShowBackToQwant() && [
             <BackToQwantButton key="back-to-qwant" />,
           ]}
         >
