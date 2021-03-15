33c04512bf32ec6534f21b2956b5494e03cf0753
bbecquet <benjamin.becquet@gmail.com>
2021-03-15 11:21:02 +0100

    Product Drawer integration

--- src/panel/menu/ProductsDrawer.jsx
+++ src/panel/menu/ProductsDrawer.jsx
@@ -2,29 +2,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { CloseButton, Flex } from 'src/components/ui';
+import ProductCard from './ProductCard';
 
 const ProductsDrawer = ({ close }) => {
-  return <div className="menu">
+  return <div className="menu productsDrawer">
     <div className="menu__overlay" onClick={close} />
 
     <div className="menu__panel">
       <Flex className="menu-top u-mb-l">
         <CloseButton circle onClick={close} />
       </Flex>
-      <h3 className="u-text--heading3">{_('Products for everyday life', 'menu')}</h3>
-      <div className="products">
-        <div className="product">
-          {_('Search', 'menu')}
+      <div className="menu-content">
+        <h3 className="u-text--heading3 u-mb-xl-2 u-center">
+          {_('Products for everyday life', 'menu')}
+        </h3>
+        <div className="products">
+          <ProductCard
+            logo="./statics/images/products/qwant-search.svg"
+            title={_('Search', 'products')}
+            desc={_('Responsible research that respects your privacy.', 'products')}
+            link={_('Open Search', 'products')}
+            href="https://qwant.com"
+          />
+          <ProductCard
+            logo="./statics/images/products/qwant-maps.svg"
+            title={_('Maps', 'products')}
+            desc={_('The map department that doesn\'t track you.', 'products')}
+            link={_('Open Maps', 'products')}
+            href="https://qwant.com/maps"
+          />
+          <ProductCard
+            logo="./statics/images/products/qwant-junior.svg"
+            title={_('Junior', 'products')}
+            desc={_('Responsible research adapted to the 6-12 ans.', 'products')}
+            link={_('Open Junior', 'products')}
+            href="https://junior.qwant.com"
+          />
         </div>
-        <div className="product">
-          {_('Maps', 'menu')}
-        </div>
-        <div className="product">
-          {_('Junior', 'menu')}
-        </div>
-      </div>
-      <div className="model">
-        {_('We believe in another model', 'menu')}
+        <a href="https://qwant.com/?drawer=awareness"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="card"
+        >
+          <Flex>
+            <div className="u-mr-l">
+              <img width="100" height="100" src="./statics/images/products/web-internaute.svg" />
+            </div>
+            <div>
+              <div className="u-color--primary u-text--heading5 u-mb-s">
+                {_('We believe in another model', 'products')}
+              </div>
+              <div className="card-link">{_('Learn more', 'products')}</div>
+            </div>
+          </Flex>
+        </a>
       </div>
     </div>
   </div>;
@@ -32,7 +63,6 @@ const ProductsDrawer = ({ close }) => {
 
 ProductsDrawer.propTypes = {
   close: PropTypes.func.isRequired,
-  openProducts: PropTypes.func.isRequired,
 };
 
 export default ProductsDrawer;
