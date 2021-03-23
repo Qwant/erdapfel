9d4b590469fac05631880e918b5db9e1e19cc3ed
bbecquet <benjamin.becquet@gmail.com>
2021-03-23 15:34:04 +0100

    Layout tweaks

--- src/panel/menu/ProductCard.jsx
+++ src/panel/menu/ProductCard.jsx
@@ -6,7 +6,7 @@ const ProductCard = ({ logo, title, desc, link, href }) => {
       <img src={logo} width="48" height="48" alt="" />
     </div>
     <div className="u-color--primary u-text--heading5 u-mb-s">{title}</div>
-    <div className="u-color--secondary u-text--body1 u-mb-l">{desc}</div>
+    <div className="card-desc u-color--secondary u-text--body1 u-mb-l">{desc}</div>
     <div className="card-link">{link}</div>
   </a>;
 };
