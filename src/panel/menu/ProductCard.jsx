645796d850d0bc4478f88d4af465010e5729b26a
bbecquet <benjamin.becquet@gmail.com>
2021-03-26 18:27:56 +0100

    Adjust logo/text spacing

--- src/panel/menu/ProductCard.jsx
+++ src/panel/menu/ProductCard.jsx
@@ -10,9 +10,7 @@ const ProductCard = ({ logo, title, desc, link, href, mobileApps }) => {
   return (
     <div className="card-wrapper">
       <a className="card productCard" href={href} target="_self">
-        <div className="u-mb-s">
-          <img src={logo} width="48" height="48" alt="" />
-        </div>
+        <img className="u-mb-xxs" src={logo} width="48" height="48" alt="" />
         <div className="u-color--primary u-text--heading5 u-mb-s">{title}</div>
         <div className="card-desc u-color--secondary u-text--body1 u-mb-l">{desc}</div>
         <div className="card-link">{link}</div>
