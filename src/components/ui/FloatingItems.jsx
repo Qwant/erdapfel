76b324f20683f0f58b6f3423fecd839a55bf6e9a
xem <maxime.euziere@gmail.com>
2021-03-31 10:35:18 +0200

    QMAPS-2071 fix floatinf items on the right

--- src/components/ui/FloatingItems.jsx
+++ src/components/ui/FloatingItems.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const FloatingItems = ({ items, position }) => (
   <div
     className="floatingItems"
-    style={position === 'left' ? { paddingLeft: 12 } : { paddingRight: 12 }}
+    style={position === 'left' ? { paddingLeft: 12 } : { paddingRight: 12, right: 0 }}
   >
     {items}
   </div>
