0c128352135419d59f148802c505cadd54b4a1be
bbecquet <benjamin.becquet@gmail.com>
2021-04-02 18:03:08 +0200

    [React] Convert POI block components to functional

--- src/panel/poi/PoiBlockContainer.jsx
+++ src/panel/poi/PoiBlockContainer.jsx
@@ -14,74 +14,73 @@ import Block from 'src/panel/poi/blocks/Block';
 import Divider from 'src/components/ui/Divider';
 import Address from 'src/components/ui/Address';
 
-export default class PoiBlockContainer extends React.Component {
-  static propTypes = {
-    poi: PropTypes.object,
-    covid19Enabled: PropTypes.bool,
-  };
-
-  render() {
-    if (!this.props.poi) {
-      return null;
-    }
-    const blocks = this.props.poi.blocks || [];
-    const hourBlock = blocks.find(b => b.type === 'opening_hours');
-    const informationBlock = blocks.find(b => b.type === 'information');
-    const phoneBlock = blocks.find(b => b.type === 'phone');
-    const websiteBlock = blocks.find(b => b.type === 'website');
-    const contactBlock = blocks.find(b => b.type === 'contact');
-    const imagesBlock = blocks.find(b => b.type === 'images');
-    const recyclingBlock = blocks.find(b => b.type === 'recycling');
-    const covidBlock = blocks.find(b => b.type === 'covid19');
-    const displayCovidInfo = this.props.covid19Enabled && blocks.find(b => b.type === 'covid19');
-    const wikipedia = informationBlock
-      ? informationBlock.blocks.find(b => b.type === 'wikipedia')
-      : null;
-
-    const hasAddressBlock = this.props.poi.address && this.props.poi.subClassName !== 'latlon';
-    const hasDetailBlocks =
-      hasAddressBlock ||
-      websiteBlock ||
-      informationBlock ||
-      phoneBlock ||
-      hourBlock ||
-      recyclingBlock ||
-      contactBlock;
-
-    return (
-      <div className="poi_panel__info">
-        {wikipedia && (
-          <div className="u-mb-m">
-            <WikiBlock block={wikipedia} />
-          </div>
-        )}
-        {displayCovidInfo && (
-          <CovidBlock block={covidBlock} countryCode={this.props.poi.address.country_code} />
-        )}
-        {hasDetailBlocks && (
-          <>
-            <Divider paddingTop={0} />
-            <h3 className="u-text--smallTitle">{_('Information')}</h3>
-          </>
-        )}
-        {hasAddressBlock && (
-          <Block className="block-address" icon="map-pin" title={_('address')}>
-            <Address inline address={this.props.poi.address} omitCountry />
-          </Block>
-        )}
-        {hourBlock && <HourBlock block={hourBlock} covid19enabled={!!displayCovidInfo} />}
-        {phoneBlock && <PhoneBlock block={phoneBlock} />}
-        {websiteBlock && <WebsiteBlock block={websiteBlock} poi={this.props.poi} />}
-        {informationBlock && <InformationBlock block={informationBlock} />}
-        {recyclingBlock && <RecyclingBlock block={recyclingBlock} />}
-        {contactBlock && <ContactBlock block={contactBlock} />}
-        {imagesBlock && (
-          <>
-            <Divider />
-            <ImagesBlock block={imagesBlock} />
-          </>
-        )}
-      </div>
-    );
+const PoiBlockContainer = ({ poi, covid19Enabled }) => {
+  if (!poi) {
+    return null;
   }
-}
+
+  const blocks = poi.blocks || [];
+  const hourBlock = blocks.find(b => b.type === 'opening_hours');
+  const informationBlock = blocks.find(b => b.type === 'information');
+  const phoneBlock = blocks.find(b => b.type === 'phone');
+  const websiteBlock = blocks.find(b => b.type === 'website');
+  const contactBlock = blocks.find(b => b.type === 'contact');
+  const imagesBlock = blocks.find(b => b.type === 'images');
+  const recyclingBlock = blocks.find(b => b.type === 'recycling');
+  const covidBlock = blocks.find(b => b.type === 'covid19');
+  const displayCovidInfo = covid19Enabled && blocks.find(b => b.type === 'covid19');
+  const wikipedia = informationBlock
+    ? informationBlock.blocks.find(b => b.type === 'wikipedia')
+    : null;
+
+  const hasAddressBlock = poi.address && poi.subClassName !== 'latlon';
+  const hasDetailBlocks =
+    hasAddressBlock ||
+    websiteBlock ||
+    informationBlock ||
+    phoneBlock ||
+    hourBlock ||
+    recyclingBlock ||
+    contactBlock;
+
+  return (
+    <div className="poi_panel__info">
+      {wikipedia && (
+        <div className="u-mb-m">
+          <WikiBlock block={wikipedia} />
+        </div>
+      )}
+      {displayCovidInfo && <CovidBlock block={covidBlock} countryCode={poi.address.country_code} />}
+      {hasDetailBlocks && (
+        <>
+          <Divider paddingTop={0} />
+          <h3 className="u-text--smallTitle">{_('Information')}</h3>
+        </>
+      )}
+      {hasAddressBlock && (
+        <Block className="block-address" icon="map-pin" title={_('address')}>
+          <Address inline address={poi.address} omitCountry />
+        </Block>
+      )}
+      {hourBlock && <HourBlock block={hourBlock} covid19enabled={!!displayCovidInfo} />}
+      {phoneBlock && <PhoneBlock block={phoneBlock} />}
+      {websiteBlock && <WebsiteBlock block={websiteBlock} poi={poi} />}
+      {informationBlock && <InformationBlock block={informationBlock} />}
+      {recyclingBlock && <RecyclingBlock block={recyclingBlock} />}
+      {contactBlock && <ContactBlock block={contactBlock} />}
+      {imagesBlock && (
+        <>
+          <Divider />
+          <ImagesBlock block={imagesBlock} />
+        </>
+      )}
+    </div>
+  );
+};
+
+PoiBlockContainer.propTypes = {
+  poi: PropTypes.object,
+  covid19Enabled: PropTypes.bool,
+};
+
+export default PoiBlockContainer;
