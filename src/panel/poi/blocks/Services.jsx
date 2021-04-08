a42b984f663998c156e5feae384f2b17338b22f6
Benjamin Becquet <benjamin.becquet@gmail.com>
2021-04-08 09:32:58 +0200

    [React] Convert POI block components to functional (#1058)
    * Convert Services POI block to a functional compoeonent
    * Convert POI block components to functional

--- src/panel/poi/blocks/Services.jsx
+++ src/panel/poi/blocks/Services.jsx
@@ -1,33 +1,18 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import AccessibilityBlock from './Accessibility';
 import BreweryBlock from './Brewery';
 import InternetAccessBlock from './InternetAccess';
 
-export default class Services extends React.Component {
-  static propTypes = {
-    accessibility: PropTypes.object,
-    internetAccess: PropTypes.object,
-    brewery: PropTypes.object,
-  };
+const Services = ({ accessibility, internetAccess, brewery }) => {
+  const [isCollapsed, setIsCollapsed] = useState(true);
 
-  state = {
-    isCollapsed: true,
-  };
-
-  expandCollapse = () => {
-    this.setState(state => ({
-      isCollapsed: !state.isCollapsed,
-    }));
-  };
-
-  getTitle() {
-    if (!this.state.isCollapsed) {
+  const getTitle = () => {
+    if (!isCollapsed) {
       return '';
     }
 
-    const { accessibility, internetAccess, brewery } = this.props;
     return (
       <span>
         {accessibility && <AccessibilityBlock block={accessibility} asString />}
@@ -37,33 +22,42 @@ export default class Services extends React.Component {
         {brewery && <BreweryBlock block={brewery} asString />}
       </span>
     );
+  };
+
+  if (!accessibility && !internetAccess && !brewery) {
+    return null;
   }
 
-  render() {
-    const { accessibility, internetAccess, brewery } = this.props;
+  return (
+    <Fragment>
+      <span
+        className="poi_panel__sub_block__title"
+        onClick={() => {
+          setIsCollapsed(!isCollapsed);
+        }}
+      >
+        {getTitle()}
+        <i
+          className={classnames('icon-icon_chevron-down', 'poi_panel__block__collapse', {
+            'poi_panel__block__collapse--reversed': !isCollapsed,
+          })}
+        />
+      </span>
+      {!isCollapsed && (
+        <div className="poi_panel__service_information__container">
+          {accessibility && <AccessibilityBlock block={accessibility} />}
+          {internetAccess && <InternetAccessBlock block={internetAccess} />}
+          {brewery && <BreweryBlock block={brewery} />}
+        </div>
+      )}
+    </Fragment>
+  );
+};
 
-    if (!accessibility && !internetAccess && !brewery) {
-      return null;
-    }
+Services.propTypes = {
+  accessibility: PropTypes.object,
+  internetAccess: PropTypes.object,
+  brewery: PropTypes.object,
+};
 
-    return (
-      <Fragment>
-        <span className="poi_panel__sub_block__title" onClick={this.expandCollapse}>
-          {this.getTitle()}
-          <i
-            className={classnames('icon-icon_chevron-down', 'poi_panel__block__collapse', {
-              'poi_panel__block__collapse--reversed': !this.state.isCollapsed,
-            })}
-          />
-        </span>
-        {!this.state.isCollapsed && (
-          <div className="poi_panel__service_information__container">
-            {accessibility && <AccessibilityBlock block={accessibility} />}
-            {internetAccess && <InternetAccessBlock block={internetAccess} />}
-            {brewery && <BreweryBlock block={brewery} />}
-          </div>
-        )}
-      </Fragment>
-    );
-  }
-}
+export default Services;
