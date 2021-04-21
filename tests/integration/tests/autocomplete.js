3151aba0231fdf5b57610f67df4b1dc355013137
xem <maxime.euziere@gmail.com>
2021-04-21 18:10:49 +0200

    QMAPS-1883 comment

--- tests/integration/tests/autocomplete.js
+++ tests/integration/tests/autocomplete.js
@@ -249,7 +249,7 @@ test('Search Query', async () => {
   await page.goto(`${APP_URL}/?q=${searchQuery}`);
   const searchValue = await getInputValue(page, '#search');
 
-  // search input is filled with query
+  // search input is filled with PoI name (not the query)
   expect(searchValue).toEqual('test result 1');
 
   // app navigates to first result from autocomplete
