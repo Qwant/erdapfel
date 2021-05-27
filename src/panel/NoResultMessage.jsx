c7c8a1f5f0e40712fc6d12f7ed0d4f47c9dee80f
bbecquet <benjamin.becquet@gmail.com>
2021-05-27 10:33:51 +0200

    Add non breaking spaces before emojis

--- src/panel/NoResultMessage.jsx
+++ src/panel/NoResultMessage.jsx
@@ -4,9 +4,12 @@ import React from 'react';
 const NoResultMessage = () => {
   return (
     <>
-      <p className="u-center u-mb-xs u-text--smallTitle">
-        {_('Sorry, we could not find this place 🏝️', 'suggest')}
-      </p>
+      <p
+        className="u-center u-mb-xs u-text--smallTitle"
+        dangerouslySetInnerHTML={{
+          __html: _('Sorry, we could not find this place&nbsp;🏝️', 'suggest'),
+        }}
+      />
       <p className="u-center u-text--subtitle">
         {_(
           'Please try to correct your query or rewrite it with more details about the location (city, country, …)',
