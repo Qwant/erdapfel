5b32626b00dbe033625d180935a4fda3fca63cac
bbecquet <benjamin.becquet@gmail.com>
2021-05-27 16:55:10 +0200

    Reveal phone number after click for PJ/desktop

--- src/panel/poi/blocks/Phone.jsx
+++ src/panel/poi/blocks/Phone.jsx
@@ -1,12 +1,35 @@
 /* globals _ */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Block from 'src/panel/poi/blocks/Block';
+import { isFromPagesJaunes } from 'src/libs/pois';
+import { useDevice } from 'src/hooks';
 
-const PhoneBlock = ({ block }) => {
+const PhoneBlock = ({ block, poi }) => {
+  const { isMobile } = useDevice();
+  const [isHidden, setHidden] = useState(true);
+
+  useEffect(() => {
+    setHidden(!isMobile && isFromPagesJaunes(poi));
+  }, [poi, isMobile]);
+
+  const onClick = () => {
+    if (isHidden) {
+      setHidden(false);
+    } else {
+      window.open(`${block.url}`, '_self');
+    }
+  };
+
   return (
-    <Block className="block-phone" icon="icon_phone" title={_('phone')} href={block.url}>
-      {block.local_format}
+    <Block
+      className="block-phone"
+      icon="icon_phone"
+      title={_('phone')}
+      onClick={isHidden ? onClick : null}
+      href={isHidden ? null : block.url}
+    >
+      {isHidden ? _('Show the number') : block.local_format}
     </Block>
   );
 };
