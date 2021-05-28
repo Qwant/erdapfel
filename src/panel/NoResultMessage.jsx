5e48e4a4f8b66eb01ea366e1d9803ef90e3d4f4d
bbecquet <benjamin.becquet@gmail.com>
2021-05-28 16:14:14 +0200

    Rebase

--- src/panel/NoResultMessage.jsx
+++ src/panel/NoResultMessage.jsx
@@ -1,22 +1,35 @@
-/* globals _ */
 import React from 'react';
+import { Button } from 'src/components/ui';
+import { useI18n } from 'src/hooks';
+import { IconExternalLink } from 'src/components/ui/icons';
 
 const NoResultMessage = () => {
+  const { getLocalizedUrl, _ } = useI18n();
+  const helpCenterLink = getLocalizedUrl('helpEditData');
+
   return (
-    <>
+    <div className="u-center">
       <p
-        className="u-center u-mb-xs u-text--smallTitle"
+        className="u-mb-xs u-text--smallTitle"
         dangerouslySetInnerHTML={{
           __html: _('Sorry, we could not find this place&nbsp;🏝️', 'suggest'),
         }}
       />
-      <p className="u-center u-text--subtitle">
+      <p className="u-text--subtitle u-mb-s">
         {_(
           'Please try to correct your query or rewrite it with more details about the location (city, country, …)',
           'suggest'
         )}
       </p>
-    </>
+      <Button
+        href={helpCenterLink}
+        variant="tertiary"
+        icon={<IconExternalLink width={16} />}
+        onMouseDown={() => window.open(helpCenterLink)}
+      >
+        {_('Add a missing place on the map')}
+      </Button>
+    </div>
   );
 };
 
