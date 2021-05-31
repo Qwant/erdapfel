f1b1939ef507df870b0456f1e0f32e7a05c33941
Benjamin Becquet <benjamin.becquet@gmail.com>
2021-05-31 17:47:17 +0200

    Change error message when no matching point in area (#1102)
    * Change error message when no matching point in area
    * Nicer emoji :)

--- src/panel/category/CategoryPanelError.jsx
+++ src/panel/category/CategoryPanelError.jsx
@@ -1,23 +1,43 @@
 /* global _ */
 import React from 'react';
+import { Button } from 'src/components/ui';
 
 const geoloc = e => {
   e.preventDefault();
   document.querySelector('.mapboxgl-ctrl-geolocate').click();
 };
 
+const zoomOut = e => {
+  e.preventDefault();
+  document.querySelector('.map_control_group__button__zoom.icon-minus').click();
+};
+
 const CategoryPanelError = ({ zoomIn }) => {
+  const { title, message, action } = zoomIn
+    ? {
+        title: _('No results found.'),
+        message: _('Please zoom in the map to see the results for this category.', 'categories'),
+        action: (
+          <Button onClick={geoloc} href="#">
+            <span className="icon-pin_geoloc" /> {_('Search around my position', 'categories')}
+          </Button>
+        ),
+      }
+    : {
+        title: _("Hmm, looks like a no-man's land 🏜️", 'categories'),
+        message: _('We found no place matching your query in this area.', 'categories'),
+        action: (
+          <Button onClick={zoomOut} variant="tertiary">
+            {_('Get some height', 'categories')}
+          </Button>
+        ),
+      };
+
   return (
-    <div className="category__panel__error">
-      <p className="u-mb-xs u-text--smallTitle">{_('No results found.')}</p>
-      <p className="u-mb-l">
-        {zoomIn
-          ? _('Please zoom in the map to see the results for this category.', 'categories')
-          : _('Please zoom out of the map.', 'categories')}
-      </p>
-      <a onClick={geoloc} href="#">
-        <span className="icon-pin_geoloc" /> {_('Search around my position', 'categories')}
-      </a>
+    <div className="category__panel__error u-center">
+      <p className="u-mb-xs u-text--smallTitle">{title}</p>
+      <p className="u-mb-s">{message}</p>
+      {action}
     </div>
   );
 };
