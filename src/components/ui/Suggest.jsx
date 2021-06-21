6fc5ea764614b6606ec05a6ec2d786178d0dcf7b
Benjamin Becquet <benjamin.becquet@gmail.com>
2021-06-21 17:04:48 +0200

    Change user feedback wording again (#1133)

--- src/components/ui/Suggest.jsx
+++ src/components/ui/Suggest.jsx
@@ -164,7 +164,7 @@ const Suggest = ({
               <UserFeedbackYesNo
                 questionId="suggest"
                 context={encodeURIComponent(value) + document.location.hash}
-                question={_('Satisfied with the proposed results?')}
+                question={_('Satisfied with the results?')}
               />
             )}
           </div>,
