028aefb679e5fe03dd3b402edc02e6e68bb30737
xem <maxime.euziere@gmail.com>
2021-06-28 15:54:20 +0200

    QMAPS-2178 start adding popup buttons (todo: visual favorites toggle, a bit of refactoring)

--- src/components/PoiPopup.jsx
+++ src/components/PoiPopup.jsx
@@ -1,8 +1,46 @@
 import React from 'react';
 import PoiItem from './PoiItem';
 import { fire } from 'src/libs/customEvents';
+import ActionButtons from '../panel/poi/ActionButtons';
+import nconf from '@qwant/nconf-getter';
+import Telemetry from '../libs/telemetry';
+import { addToFavorites, isInFavorites, removeFromFavorites } from '../adapters/store';
 
 const PoiPopup = ({ poi }) => {
+  const isDirectionActive = nconf.get().direction.enabled;
+
+  const openDirection = () => {
+    Telemetry.sendPoiEvent(poi, 'itinerary');
+    window.app.navigateTo('/routes/', { poi });
+  };
+
+  const onClickPhoneNumber = () => {
+    const source = poi.meta && poi.meta.source;
+    if (source) {
+      Telemetry.sendPoiEvent(
+        poi,
+        'phone',
+        Telemetry.buildInteractionData({
+          id: poi.id,
+          source,
+          template: 'single',
+          zone: 'detail',
+          element: 'phone',
+        })
+      );
+    }
+  };
+
+  const toggleStorePoi = () => {
+    const inFavorites = isInFavorites(poi);
+    Telemetry.sendPoiEvent(poi, 'favorite', { stored: !inFavorites });
+    if (inFavorites) {
+      removeFromFavorites(poi);
+    } else {
+      addToFavorites(poi);
+    }
+  };
+
   return (
     <div
       className="poi_popup"
@@ -13,7 +51,17 @@ const PoiPopup = ({ poi }) => {
         fire('close_popup');
       }}
     >
-      <PoiItem poi={poi} withOpeningHours withImage inList />
+      <div className="u-mb-s">
+        <PoiItem poi={poi} withOpeningHours withImage inList />
+      </div>
+      <ActionButtons
+        poi={poi}
+        isDirectionActive={isDirectionActive}
+        openDirection={openDirection}
+        onClickPhoneNumber={onClickPhoneNumber}
+        isPoiInFavorite={isInFavorites(poi)}
+        toggleStorePoi={toggleStorePoi}
+      />
     </div>
   );
 };
