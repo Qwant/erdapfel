c027b93bc150865e7a3686d598c31669561180d2
bbecquet <benjamin.becquet@gmail.com>
2021-06-29 17:39:04 +0200

    Add a autoHideDelay prop to Alert

--- src/components/ui/Alert.jsx
+++ src/components/ui/Alert.jsx
@@ -1,27 +1,29 @@
 import React, { useEffect, useState, useRef } from 'react';
 import cs from 'classnames';
-import { string, node } from 'prop-types';
+import { string, node, number } from 'prop-types';
 import { CloseButton } from './index';
 import { IconCheckboxCircle } from './icons';
 import { GREEN_DARK } from 'src/libs/colors';
 
-const Alert = ({ className = '', children, type }) => {
+const Alert = ({ className = '', autoHideDelay, children, type }) => {
   const [hidden, setHidden] = useState(false);
   const alertRef = useRef();
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      // Animate is not supported on old iOS
-      if (alertRef.current && alertRef.current.animate) {
-        alertRef.current.animate([{ opacity: 1 }, { opacity: 0 }], 300).onfinish = () => {
+    if (autoHideDelay) {
+      const timer = setTimeout(() => {
+        // Animate is not supported on old iOS
+        if (alertRef.current && alertRef.current.animate) {
+          alertRef.current.animate([{ opacity: 1 }, { opacity: 0 }], 300).onfinish = () => {
+            setHidden(true);
+          };
+        } else {
           setHidden(true);
-        };
-      } else {
-        setHidden(true);
-      }
-    }, 3000);
-    return () => clearTimeout(timer);
-  }, [alertRef]);
+        }
+      }, autoHideDelay);
+      return () => clearTimeout(timer);
+    }
+  }, [alertRef, autoHideDelay]);
 
   if (hidden) {
     return null;
@@ -44,6 +46,7 @@ Alert.propTypes = {
   className: string,
   children: node.isRequired,
   type: string,
+  autoHideDelay: number,
 };
 
 export default Alert;
