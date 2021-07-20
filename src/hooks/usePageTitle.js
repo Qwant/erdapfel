77fa18433236909fcd236bf5827630867d85215f
bbecquet <benjamin.becquet@gmail.com>
2021-07-20 18:11:55 +0200

    Add envName to dynamic page title

--- src/hooks/usePageTitle.js
+++ src/hooks/usePageTitle.js
@@ -1,12 +1,15 @@
 import { useEffect } from 'react';
+import { useConfig } from './useConfig';
 
 export const usePageTitle = (title, suffix = 'Qwant Maps') => {
+  const envName = useConfig('envName');
+
   useEffect(() => {
     const previousTitle = document.title;
-    document.title = [title, suffix].filter(i => i).join(' - ');
+    document.title = [title, suffix, envName].filter(i => i).join(' - ');
 
     return () => {
       document.title = previousTitle;
     };
-  }, [title, suffix]);
+  }, [title, suffix, envName]);
 };
