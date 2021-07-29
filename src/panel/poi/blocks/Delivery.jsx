264f48779a0ca5b0ed5579ac99268262babd172f
bbecquet <benjamin.becquet@gmail.com>
2021-07-29 13:26:53 +0200

    Add Delivery detail block

---
+++ src/panel/poi/blocks/Delivery.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Block from './Block';
+import { useI18n } from 'src/hooks';
+import { IconEBike } from 'src/components/ui/icons';
+import { ACTION_BLUE_BASE } from 'src/libs/colors';
+
+const DeliveryBlock = ({ block }) => {
+  const { _ } = useI18n();
+
+  const modes = [
+    { key: 'click_and_collect', label: _('Click & collect') },
+    { key: 'delivery', label: _('Delivery') },
+    { key: 'takeaway', label: _('Take away') },
+  ];
+
+  return (
+    <Block icon={<IconEBike fill={ACTION_BLUE_BASE} width={20} />}>
+      {modes
+        .filter(mode => block[mode.key] === 'yes')
+        .map(mode => mode.label)
+        .join(' ⋅ ')}
+    </Block>
+  );
+};
+
+export default DeliveryBlock;
