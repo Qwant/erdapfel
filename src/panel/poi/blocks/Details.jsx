cf050d22d88d32a08d6e07a54202f9869caf7559
xem <maxime.euziere@gmail.com>
2021-08-23 18:00:28 +0200

    QMAPS-434 add poi stars for lodging and restaurants (#1177)
    * QMAPS-434 cleanup, add poi stars for lodging and restaurants
    
    * QMAPS-434 mr fixes
    
    * QMAPS-434 mr fix
    
    * QMAPS-434 retrieve stars from API

--- src/panel/poi/blocks/Details.jsx
+++ src/panel/poi/blocks/Details.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
+import StarsBlock, { hasStars } from './Stars';
 import AccessibilityBlock from './Accessibility';
-// import BreweryBlock from './Brewery';
 import InternetAccessBlock from './InternetAccess';
 import DeliveryBlock, { hasActiveDeliveryModes } from './Delivery';
 import { Divider } from 'src/components/ui';
 import { findBlock } from 'src/libs/pois';
 import { useI18n } from 'src/hooks';
+import poiSubClass from 'src/mapbox/poi_subclass';
 
 const DetailsBlock = ({ poi }) => {
   const { _ } = useI18n();
-
+  const subclass = poiSubClass(poi.subClassName);
   const accessibility = findBlock(poi.blocks, 'accessibility');
   const internetAccess = findBlock(poi.blocks, 'internet_access');
   const delivery = findBlock(poi.blocks, 'delivery');
-  // const brewery = findBlock(poi.blocks, 'brewery');
-  // {brewery && <BreweryBlock block={brewery} />}
+  const stars = findBlock(poi.blocks, 'stars');
 
-  if (!accessibility && !internetAccess && !hasActiveDeliveryModes(delivery)) {
+  if (!accessibility && !internetAccess && !hasStars(stars) && !hasActiveDeliveryModes(delivery)) {
     return null;
   }
 
@@ -25,6 +25,7 @@ const DetailsBlock = ({ poi }) => {
       <Divider paddingTop={0} />
       <h3 className="u-text--smallTitle u-mb-xs">{_('Details')}</h3>
       <div className="poi_panel__fullWidth u-mb-s">
+        {stars && <StarsBlock block={stars} subclass={subclass} />}
         {accessibility && <AccessibilityBlock block={accessibility} />}
         {internetAccess && <InternetAccessBlock block={internetAccess} />}
         {hasActiveDeliveryModes(delivery) && <DeliveryBlock block={delivery} />}
---
+++ src/panel/poi/blocks/Stars.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Block from './Block';
+import { IconStar } from 'src/components/ui/icons';
+import { ACTION_BLUE_BASE } from 'src/libs/colors';
+import { capitalizeFirst } from 'src/libs/string';
+import { useI18n } from 'src/hooks';
+
+const StarsBlock = ({ block, subclass }) => {
+  const { _, _n } = useI18n();
+
+  if (!hasStars(block)) {
+    return null;
+  }
+
+  if (block.ratings[0].nb_stars && block.ratings[0].nb_stars > 0) {
+    return (
+      <Block simple icon={<IconStar fill={ACTION_BLUE_BASE} width={20} />}>
+        {capitalizeFirst(
+          _n(
+            '{subclass} with %d star',
+            '{subclass} with %d stars',
+            block.ratings[0].nb_stars,
+            'poi',
+            { subclass }
+          )
+        )}
+      </Block>
+    );
+  }
+
+  return (
+    <Block simple icon={<IconStar fill={ACTION_BLUE_BASE} width={20} />}>
+      {_('Starred {subclass}', 'poi', { subclass })}
+    </Block>
+  );
+};
+
+export default StarsBlock;
+
+export const hasStars = stars => stars?.ratings?.[0]?.has_stars === 'yes' || false;
