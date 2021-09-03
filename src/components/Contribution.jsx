f1aa9f44c24b669fc5a9def56930514e8e1a554f
xem <maxime.euziere@gmail.com>
2021-09-03 10:28:20 +0200

    QMAPS-2226 Use Contribute block for OSM and PJ (#1182)

---
+++ src/components/Contribution.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import Telemetry from 'src/libs/telemetry';
+import { Flex } from 'src/components/ui';
+import { isFromOSM } from 'src/libs/pois';
+import classnames from 'classnames';
+import { useI18n } from 'src/hooks';
+
+const Contribution = ({ poi }) => {
+  const { _ } = useI18n();
+
+  if (!poi.meta) {
+    return null;
+  }
+
+  const { source_url, contribute_url } = poi.meta;
+  const sendTelemetryEvent = event => () => Telemetry.sendPoiEvent(poi, event);
+  return (
+    <Flex
+      className={classnames('contribute', isFromOSM(poi) ? 'contribute--osm' : 'contribute--pj')}
+    >
+      <div className="contribute__logo" />
+      <div className="u-text--caption">
+        <p>
+          {isFromOSM(poi)
+            ? _('Qwant Maps uses OpenStreetMap data.')
+            : `${_('In partnership with')} PagesJaunes.`}
+        </p>
+        <div>
+          <a
+            className="u-mr-xl"
+            href={source_url}
+            rel="noopener noreferrer"
+            target="_blank"
+            onClick={sendTelemetryEvent(isFromOSM(poi) ? 'osm_view' : 'pj_view')}
+          >
+            {_('View')}
+          </a>
+          <a
+            href={contribute_url}
+            rel="noopener noreferrer"
+            target="_blank"
+            onClick={sendTelemetryEvent(isFromOSM(poi) ? 'osm_edit' : 'pj_edit')}
+          >
+            {_('Edit')}
+          </a>
+        </div>
+      </div>
+    </Flex>
+  );
+};
+
+export default Contribution;
--- src/panel/poi/PoiPanelContent.jsx
+++ src/panel/poi/PoiPanelContent.jsx
@@ -3,9 +3,8 @@ import PropTypes from 'prop-types';
 import Telemetry from 'src/libs/telemetry';
 import ActionButtons from './ActionButtons';
 import PoiBlockContainer from './PoiBlockContainer';
-import OsmContribution from 'src/components/OsmContribution';
+import Contribution from 'src/components/Contribution';
 import CategoryList from 'src/components/CategoryList';
-import { isFromPagesJaunes, isFromOSM } from 'src/libs/pois';
 import { fire } from 'src/libs/customEvents';
 import PoiItem from 'src/components/PoiItem';
 import { Flex, Divider } from 'src/components/ui';
@@ -88,13 +87,7 @@ const PoiPanelContent = ({ poi }) => {
       </div>
       <div className="poi_panel__fullContent">
         <PoiBlockContainer poi={poi} />
-        {isFromPagesJaunes(poi) && (
-          <div className="poi_panel__info-partnership u-text--caption u-mb-s">
-            {_('In partnership with')}
-            <img src="./statics/images/pj.svg" alt="PagesJaunes" width="80" height="16" />
-          </div>
-        )}
-        {isFromOSM(poi) && <OsmContribution poi={poi} />}
+        <Contribution poi={poi} />
         <Divider paddingTop={0} className="poi_panel__fullWidth" />
         <h3 className="u-text--smallTitle u-mb-s">{_('Search around this place', 'poi')}</h3>
         <CategoryList className="poi_panel__categories u-mb-s" limit={4} />
