48527f1c385af39866744e4beee50e73d83a91ca
xem <maxime.euziere@gmail.com>
2021-09-07 09:53:35 +0200

    QMAPS-2285 pr fixes

--- bin/middlewares/unsupported_browser.js
+++ bin/middlewares/unsupported_browser.js
@@ -1,25 +1,24 @@
-function iOSversion(userAgent) {
+function isOldIE(userAgent) {
+  return userAgent.match('Trident'); // Test for IE <= 11
+}
+
+function isOldiOS(userAgent) {
   if (/iP(hone|od|ad)/.test(userAgent)) {
     const v = userAgent.match(/OS (\d+)_(\d+)_?(\d+)?/);
-    return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
+    if (v && v[1]) {
+      return parseInt(v[1], 10) < 10; // test if major iOS version is < 10
+    }
   }
 }
 
 function isUnsupported(userAgent = '') {
-  // iOS < 10 detection
-  if (iOSversion(userAgent)[0] < 10) {
-    return true;
-  }
-
-  // old IE detection
-  if (userAgent.match('Trident')) {
+  if (isOldIE(userAgent) || isOldiOS(userAgent)) {
     return true;
   }
 }
 
 module.exports = function (config) {
   const redirect = config.server.unsupportedBrowsers.redirect;
-
   return function (req, res, next) {
     if (redirect && isUnsupported(req.headers['user-agent'])) {
       res.redirect(302, config.system.baseUrl + 'unsupported');
