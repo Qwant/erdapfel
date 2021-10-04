b0f7cc0317ccb39cf95fe4fd66521fd171891f43
bbecquet <benjamin.becquet@gmail.com>
2021-10-04 15:32:24 +0200

    Replace Button in ActionButtons

--- src/panel/poi/ActionButtons.jsx
+++ src/panel/poi/ActionButtons.jsx
@@ -2,17 +2,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Telemetry from 'src/libs/telemetry';
-import { Flex, ShareMenu, Button } from 'src/components/ui';
+import { ShareMenu } from 'src/components/ui';
+import { IconHeart, IconHeartFill } from 'src/components/ui/icons';
+import { PINK_DARK } from 'src/libs/colors';
 import {
-  IconHeart,
-  IconHeartFill,
+  Stack,
+  Button,
+  IconDirection,
+  IconPhone,
   IconCalendar,
   IconFileList,
-  IconPhone,
-  IconDirection,
   IconShare,
-} from 'src/components/ui/icons';
-import { PINK_DARK } from 'src/libs/colors';
+} from '@qwant/qwant-ponents';
 
 const TransactionalButton = ({ poi }) => {
   const { booking_url, appointment_url, quotation_request_url } =
@@ -57,12 +58,15 @@ const TransactionalButton = ({ poi }) => {
 
   return (
     <Button
-      icon={<Icon width={16} height={16} />}
       href={url}
       rel="noopener noreferrer external"
       title={label}
       onClick={sendTelemetryEvent}
-    />
+      variant="secondary"
+      pictoButton
+    >
+      <Icon />
+    </Button>
   );
 };
 
@@ -86,15 +90,15 @@ const ActionButtons = ({
   const favoriteColor = isPoiInFavorite ? PINK_DARK : null;
 
   return (
-    <Flex className="poi_panel__actions">
+    <Stack className="poi_panel__actions" horizontal gap="xs">
       {isDirectionActive && (
         <Button
           className="poi_panel__action__direction"
           variant="primary"
-          icon={<IconDirection width={15} height={16} />}
           onClick={openDirection}
           title={_('Directions', 'poi panel')}
         >
+          <IconDirection />
           {_('Directions', 'poi panel')}
         </Button>
       )}
@@ -102,30 +106,34 @@ const ActionButtons = ({
       {poi?.blocksByType?.phone && (
         <Button
           className="poi_panel__action__phone"
+          variant="secondary"
+          pictoButton
           onClick={onClickPhoneNumber}
-          icon={<IconPhone width={16} height={16} />}
           href={poi.blocksByType.phone.url}
           rel="noopener noreferrer external"
           title={_('Call', 'poi panel')}
-        />
+        >
+          <IconPhone />
+        </Button>
       )}
 
       <TransactionalButton poi={poi} />
 
       <Button
         className="poi_panel__action__favorite"
+        variant="secondary"
+        pictoButton
         data-active={isPoiInFavorite}
         title={_('Favorites', 'poi panel')}
         onClick={toggleStorePoi}
         style={{ borderColor: favoriteColor }}
-        icon={
-          isPoiInFavorite ? (
-            <IconHeartFill width={16} height={16} color={favoriteColor} />
-          ) : (
-            <IconHeart width={16} height={16} />
-          )
-        }
-      />
+      >
+        {isPoiInFavorite ? (
+          <IconHeartFill width={16} height={16} fill={favoriteColor} />
+        ) : (
+          <IconHeart width={16} height={16} fill="currentColor" />
+        )}
+      </Button>
 
       <ShareMenu
         url={window.location.toString()}
@@ -135,13 +143,16 @@ const ActionButtons = ({
         {openMenu => (
           <Button
             className="poi_panel__action__share"
+            variant="secondary"
+            pictoButton
             title={_('Share', 'poi panel')}
-            icon={<IconShare width={16} height={16} />}
             onClick={e => onShareClick(e, openMenu)}
-          />
+          >
+            <IconShare />
+          </Button>
         )}
       </ShareMenu>
-    </Flex>
+    </Stack>
   );
 };
 
