767a9acee6bd8fa9df272f5412ab960d493ce0cb
xem <maxime.euziere@gmail.com>
2021-10-18 11:09:35 +0200

    QMAPS-2312 survey integration

--- src/components/Survey.jsx
+++ src/components/Survey.jsx
@@ -1,10 +1,9 @@
-import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import { Notification, Text } from '@qwant/qwant-ponents';
 import React, { useState, useEffect } from 'react';
 import { useDevice, useSurvey } from 'src/hooks';
 
-const Survey = ({ home = false }) => {
+const Survey = () => {
   const [enabled, setEnabled] = useState(true);
   const [survey, setSurvey] = useState(null);
   const { isMobile } = useDevice();
@@ -25,22 +24,19 @@ const Survey = ({ home = false }) => {
   return (
     <div>
       {survey && enabled && (
-        <div className={classnames('SurveyNotification', home && 'SurveyNotificationBottom')}>
-          <Notification
-            title={survey.person_name}
-            titleSecondary={survey.title}
-            icon={survey.image}
-            url={survey.url}
-            buttonLabel={survey.cta}
-            onClose={onClose}
-            onClick={() => {}}
-            mobile={isMobile}
-          >
-            <Text typo="body-2" color="primary">
-              desc
-            </Text>
-          </Notification>
-        </div>
+        <Notification
+          title={survey.person_name}
+          titleSecondary={survey.title}
+          icon={survey.image}
+          url={survey.url}
+          buttonLabel={survey.cta}
+          onClose={onClose}
+          mobile={isMobile}
+        >
+          <Text typo="body-2" color="primary">
+            {survey.desc}
+          </Text>
+        </Notification>
       )}
     </div>
   );
--- src/hooks/useSurvey.js
+++ src/hooks/useSurvey.js
@@ -1,13 +1,14 @@
 import { useDevice, useConfig } from 'src/hooks';
 
 export const useSurvey = () => {
-  const { testGroupPer } = useConfig('testGroupPer');
+  const testGroupPer = useConfig('testGroupPer');
   const { isMobile } = useDevice();
   const { surveyApiUrl } = useConfig('survey');
   const params = new URLSearchParams();
   params.set('website', 'maps');
   params.set('locale', window.preferedLanguage.locale); // case-insensitive: the API allows 'fr_FR' or 'fr_fr' formats.
-  params.set('tgp', testGroupPer || 50); // undefined?
+  params.set('tgp', testGroupPer);
+  console.log(testGroupPer);
   params.set('device', isMobile ? 'mobile' : 'desktop');
   return surveyApiUrl + '?' + params.toString();
 };
