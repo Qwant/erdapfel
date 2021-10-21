2bd4162435f4b478503bb13f3318c089cee3941f
bbecquet <benjamin.becquet@gmail.com>
2021-10-21 16:32:59 +0200

    fixup! Replace Flex in MobileRouteDetails

--- src/panel/direction/MobileRouteDetails.jsx
+++ src/panel/direction/MobileRouteDetails.jsx
@@ -1,8 +1,7 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import RoadMap from './RoadMap';
 import { CloseButton, Divider } from 'src/components/ui';
-import classnames from 'classnames';
-import { Button, Flex } from '@qwant/qwant-ponents';
+import { Box, Button, Flex } from '@qwant/qwant-ponents';
 import RouteSummaryInfo from './RouteSummaryInfo';
 import { useI18n } from 'src/hooks';
 
@@ -15,13 +14,12 @@ const MobileRouteDetails = ({
   toggleDetails,
   openPreview,
 }) => {
-  const panelElement = useRef(null);
   const { _ } = useI18n();
 
   return (
-    <div ref={panelElement} className={classnames('mobile-route-details')}>
+    <div className="mobile-route-details">
       <div className="mobile-route-details-header">
-        <div className="mobile-route-details-header-content">
+        <Box py="l" px="s">
           <Flex between>
             <RouteSummaryInfo route={route} vehicle={vehicle} />
             <CloseButton position="topRight" onClick={() => toggleDetails(id)} />
@@ -33,13 +31,14 @@ const MobileRouteDetails = ({
                 openPreview(id);
               }}
               variant="primary"
-              style={{ width: '100%', marginTop: 20 }}
+              full
+              mt="l"
             >
               <img className="u-mr-xxs" src="./statics/images/direction_icons/guide.svg" />
               <span className="u-firstCap">{_('step by step', 'direction')}</span>
             </Button>
           )}
-        </div>
+        </Box>
 
         <Divider paddingTop={0} paddingBottom={0} />
       </div>
