9a515cdba59f45a6b44b8ab702201829ff275830
bbecquet <benjamin.becquet@gmail.com>
2021-10-21 16:14:22 +0200

    Replace Flex in MobileRouteDetails

--- src/panel/direction/MobileRouteDetails.jsx
+++ src/panel/direction/MobileRouteDetails.jsx
@@ -1,8 +1,8 @@
 import React, { useRef } from 'react';
 import RoadMap from './RoadMap';
-import { CloseButton, Divider, Flex } from 'src/components/ui';
+import { CloseButton, Divider } from 'src/components/ui';
 import classnames from 'classnames';
-import { Button } from '@qwant/qwant-ponents';
+import { Button, Flex } from '@qwant/qwant-ponents';
 import RouteSummaryInfo from './RouteSummaryInfo';
 import { useI18n } from 'src/hooks';
 
@@ -22,7 +22,7 @@ const MobileRouteDetails = ({
     <div ref={panelElement} className={classnames('mobile-route-details')}>
       <div className="mobile-route-details-header">
         <div className="mobile-route-details-header-content">
-          <Flex alignItems="flex-start" justifyContent="space-between">
+          <Flex between>
             <RouteSummaryInfo route={route} vehicle={vehicle} />
             <CloseButton position="topRight" onClick={() => toggleDetails(id)} />
           </Flex>
