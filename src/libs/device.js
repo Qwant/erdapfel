11b0aea2fdd311315ddbf627f15442c06f80d33c
Benjamin Becquet <benjamin.becquet@gmail.com>
2021-10-26 10:25:43 +0200

    Maintain device context in dedicated provider (#1222)

--- src/libs/device.js
+++ src/libs/device.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export const mobileDeviceMediaQuery = window.matchMedia('(max-width: 640px)');
 
@@ -7,3 +7,21 @@ export function isMobileDevice() {
 }
 
 export const DeviceContext = React.createContext({ isMobile: isMobileDevice() });
+
+export const DeviceProvider = ({ children }) => {
+  const [isMobile, setIsMobile] = useState(isMobileDevice());
+
+  useEffect(() => {
+    const deviceChanged = ({ matches: isMobile }) => {
+      setIsMobile(isMobile);
+    };
+
+    mobileDeviceMediaQuery.addEventListener('change', deviceChanged);
+
+    return () => {
+      mobileDeviceMediaQuery.removeEventListener('change', deviceChanged);
+    };
+  }, []);
+
+  return <DeviceContext.Provider value={{ isMobile }}>{children}</DeviceContext.Provider>;
+};
