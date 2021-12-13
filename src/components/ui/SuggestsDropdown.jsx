705ea5764c742f40aad055cb74b08fb1ad2cc47f
xem <maxime.euziere@gmail.com>
2021-12-13 17:31:04 +0100

    QMAPS-2358 history-suggest wip

--- src/components/ui/SuggestsDropdown.jsx
+++ src/components/ui/SuggestsDropdown.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useState } from 'react';
 import classnames from 'classnames';
 import { object, func, string, arrayOf } from 'prop-types';
 import SuggestItem from './SuggestItem';
@@ -9,25 +9,70 @@ import { getHistoryPrompt } from 'src/adapters/search_history';
 const SuggestsDropdown = ({ className = '', suggestItems, onSelect, highlighted, value }) => {
   const { _ } = useI18n();
   const searchHistoryConfig = useConfig('searchHistory');
+  const [answer, setAnswer] = useState(null);
+
+  const historyPrompt = () => {
+    if (answer === null) {
+      return (
+        <Box m="l">
+          <Heading as="h6">{_('History is available on Qwant Maps', 'history')}</Heading>
+          <Stack>
+            <Box>
+              {_(
+                'Convenient and completely private, the history will only be visible to you on this device 🙈.',
+                'history'
+              )}{' '}
+              <a href="">{_('Read more', 'history')}</a>
+            </Box>
+            <Box mt="l">
+              <Button variant="secondary" onClick={() => setAnswer(false)}>
+                {_('No thanks', 'history')}
+              </Button>
+              <Button ml="l" onClick={() => setAnswer(true)}>
+                {_('Enable history', 'history')}
+              </Button>
+            </Box>
+          </Stack>
+        </Box>
+      );
+    }
+
+    if (answer === true) {
+      return (
+        <Box m="l">
+          <Heading as="h6">{_('Well done, the history is activated!', 'history')}</Heading>
+          <Stack>
+            <Box>
+              {_(
+                'You can find and manage your complete history at any time in the menu',
+                'history'
+              )}
+            </Box>
+          </Stack>
+        </Box>
+      );
+    }
+
+    if (answer === false) {
+      return (
+        <Box m="l">
+          <Heading as="h6">{_('No worries, history is disabled', 'history')}</Heading>
+          <Stack>
+            <Box>
+              {_(
+                'You can change your mind at any time and manage the activation of the history in the menu.',
+                'history'
+              )}
+            </Box>
+          </Stack>
+        </Box>
+      );
+    }
+  };
 
   // Focused and empty field, unanswered prompt, history feature enabled: show history prompt
   return searchHistoryConfig?.enabled && value === '' && getHistoryPrompt() === null ? (
-    <Box m="l">
-      <Heading as="h6">{_('History is available on Qwant Maps', 'history')}</Heading>
-      <Stack>
-        <Box>
-          {_(
-            'Convenient and completely private, the history will only be visible to you on this device 🙈.',
-            'history'
-          )}{' '}
-          <a href="">{_('Read more', 'history')}</a>
-        </Box>
-        <Box mt="l">
-          <Button variant="secondary">{_('No thanks', 'history')}</Button>
-          <Button ml="l">{_('Enable history', 'history')}</Button>
-        </Box>
-      </Stack>
-    </Box>
+    historyPrompt()
   ) : (
     // Focused field and (answered prompt or history feature disabled): show suggest
     <ul className={classnames('autocomplete_suggestions', className)}>
