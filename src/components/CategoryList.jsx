7151f8b165795aa77dd07aa546eebfa93b16b6a5
Olivier PASCAL <o.pascal@qwant.com>
2022-02-18 17:37:43 +0100

    QMAPS-2428 - Add TA and PJ POI in History

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,33 +1,43 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import CategoryService from 'src/adapters/category_service';
 import MainActionButton from 'src/components/ui/MainActionButton';
 import Telemetry from 'src/libs/telemetry';
 import { getLightBackground } from 'src/libs/colors';
+import { saveQuery, getHistoryEnabled } from '../adapters/search_history';
 
-const handleCategoryClick = category => {
-  Telemetry.add(Telemetry.HOME_CATEGORY, { category: category.name });
-  window.app.navigateTo(`/places/?type=${category.name}`);
+const CategoryList = ({ className, limit = Number.MAX_VALUE }) => {
+  const searchHistoryEnabled = getHistoryEnabled();
+  const handleCategoryClick = useCallback(
+    category => {
+      if (searchHistoryEnabled && category) {
+        saveQuery(category);
+      }
+      Telemetry.add(Telemetry.HOME_CATEGORY, { category: category.name });
+      window.app.navigateTo(`/places/?type=${category.name}`);
+    },
+    [searchHistoryEnabled]
+  );
+
+  return (
+    <div className={className}>
+      {CategoryService.getCategories()
+        .filter(c => c.iconName) // ignore categories used on detected intention only
+        .slice(0, limit)
+        .map(category => (
+          <MainActionButton
+            key={category.name}
+            onClick={() => handleCategoryClick(category)}
+            variant="category"
+            label={category.label}
+            icon={category.iconName}
+            iconStyle={{
+              color: category.color,
+              backgroundColor: getLightBackground(category.color),
+            }}
+          />
+        ))}
+    </div>
+  );
 };
-
-const CategoryList = ({ className, limit = Number.MAX_VALUE }) => (
-  <div className={className}>
-    {CategoryService.getCategories()
-      .filter(c => c.iconName) // ignore categories used on detected intention only
-      .slice(0, limit)
-      .map(category => (
-        <MainActionButton
-          key={category.name}
-          onClick={() => handleCategoryClick(category)}
-          variant="category"
-          label={category.label}
-          icon={category.iconName}
-          iconStyle={{
-            color: category.color,
-            backgroundColor: getLightBackground(category.color),
-          }}
-        />
-      ))}
-  </div>
-);
 
 export default CategoryList;
