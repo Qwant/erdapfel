66d67613fb1129b4c17c201bfca1ef164fd6e5a3
xem <maxime.euziere@gmail.com>
2022-04-05 10:42:54 +0200

    QMAPS-2648 itinerary logs (#1322)

--- src/panel/direction/DirectionPanel.jsx
+++ src/panel/direction/DirectionPanel.jsx
@@ -187,6 +187,7 @@ class DirectionPanel extends React.Component {
       fire('set_origin', origin);
       fire('set_destination', destination);
       const directionResponse = await DirectionApi.search(origin, destination, vehicle);
+
       // A more recent query was done in the meantime, ignore this result silently
       if (currentQueryId !== this.lastQueryId) {
         return;
