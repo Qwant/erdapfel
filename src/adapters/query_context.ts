9aeb15943b588e4cabbc27a1328d68ac17601893
xem <maxime.euziere@gmail.com>
2022-07-06 15:18:08 +0200

    QMAPS-2649 query_context

--- src/adapters/query_context.ts
+++ src/adapters/query_context.ts
@@ -3,10 +3,27 @@ import nconf from '@qwant/nconf-getter';
 const sendQueryContextHeaders = nconf.get().telemetry.sendQueryContextHeaders;
 
 // Used to provide information when an item from autocomplete is selected.
-//
 // The goal is to improve results provided by bragi based on users' selections.
+
+export type TQueryContext = {
+  term: string;
+  ranking: string;
+  lang: string | null;
+  className: string;
+  position: { lon?: number; lat?: number; zoom?: number };
+};
 export default class QueryContext {
-  constructor(term, ranking, lang = null, position = {}) {
+  term: TQueryContext['term'];
+  ranking: TQueryContext['ranking'];
+  lang: TQueryContext['lang'];
+  position: TQueryContext['position'];
+
+  constructor(
+    term: TQueryContext['term'],
+    ranking: TQueryContext['ranking'],
+    lang: TQueryContext['lang'] = null,
+    position: TQueryContext['position'] = {}
+  ) {
     this.term = term;
     this.ranking = ranking;
     this.lang = lang;
@@ -14,7 +31,7 @@ export default class QueryContext {
     this.position = position || {};
   }
 
-  static toHeaders(queryContext) {
+  static toHeaders(queryContext: TQueryContext) {
     if (!sendQueryContextHeaders || !queryContext) {
       return {};
     }
