e41fb07702338f1cd7aae00d29c4f307da5d592f
Olivier PASCAL <pascaloliv@gmail.com>
2022-07-13 10:16:44 +0200

    Add new chevron icon + port 2 component files to TS

---
+++ src/panel/direction/PublicTransportLine/index.tsx
@@ -0,0 +1,53 @@
+/* global _ */
+import React from 'react';
+import Color from 'color';
+import cx from 'classnames';
+import { components } from 'appTypes/idunn';
+
+export type PublicTransportLineProps = {
+  mode: components['schemas']['TransportMode'];
+  info?: components['schemas']['TransportInfo'];
+  showDirection?: boolean;
+};
+
+const PublicTransportLine: React.FunctionComponent<PublicTransportLineProps> = ({
+  mode,
+  info,
+  showDirection = false,
+}) => {
+  let type = 'ligne';
+  if (mode.startsWith('BUS')) {
+    type = 'bus';
+  } else if (mode.startsWith('SUBWAY')) {
+    type = 'métro';
+  } else if (mode.startsWith('TRAM')) {
+    type = 'tram';
+  } else if (mode.indexOf('TRAIN') !== -1) {
+    if (info?.num?.startsWith('RER')) {
+      type = '';
+    } else {
+      type = `train ${info?.network}`;
+    }
+  }
+  const lineColor = info?.lineColor ? Color('#' + info.lineColor) : Color('white');
+  return (
+    <>
+      <span
+        className={cx('routePtLine', { 'routePtLine--dark': lineColor.isDark() })}
+        style={{
+          backgroundColor: lineColor.hex(),
+          borderColor: lineColor.rgbNumber() === 0xffffff ? 'black' : lineColor.hex(),
+        }}
+      >
+        {type} {info?.num}
+      </span>
+      {showDirection && (
+        <span className={cx('routePtLine-direction', 'u-text--subtitle')}>
+          {_('Towards {direction}', 'direction')?.replace('{direction}', info?.direction ?? '')}
+        </span>
+      )}
+    </>
+  );
+};
+
+export default PublicTransportLine;
