b782ada2f2a44d00ed8a4dea28e656a3d3468e9c
Rémi Dupré <remi@dupre.io>
2022-10-26 14:30:02 +0200

    add feature flag features_ecoResponsible

--- src/panel/service/ServicePanelDesktop.jsx
+++ src/panel/service/ServicePanelDesktop.jsx
@@ -1,12 +1,13 @@
 import React, { Fragment, useState } from 'react';
 import { Panel, Chevron } from 'src/components/ui';
 import CategoryList from 'src/components/CategoryList';
-import { useI18n } from 'src/hooks';
+import { useConfig, useI18n } from 'src/hooks';
 import { Flex, Button } from '@qwant/qwant-ponents';
 
 const ServicePanelDesktop = () => {
   const [collapsed, setCollapsed] = useState(true);
   const { _ } = useI18n();
+  const ecoResponsible = useConfig('features').ecoResponsible;
 
   return (
     <Fragment>
@@ -20,16 +21,20 @@ const ServicePanelDesktop = () => {
           limit={collapsed ? 4 : undefined}
         />
       </Panel>
-      <Panel className="service_panel u-mb-m">
-        <h3 className="u-text--smallTitle u-mb-s">
-          {_('Eco-responsible places', 'eco service panel')}
-        </h3>
-        <CategoryList
-          className="service_panel__categories"
-          ecoResponsible={true}
-          limit={collapsed ? 4 : undefined}
-        />
-      </Panel>
+      {ecoResponsible && (
+        <>
+          <Panel className="service_panel u-mb-m">
+            <h3 className="u-text--smallTitle u-mb-s">
+              {_('Eco-responsible places', 'eco service panel')}
+            </h3>
+            <CategoryList
+              className="service_panel__categories"
+              ecoResponsible={true}
+              limit={collapsed ? 4 : undefined}
+            />
+          </Panel>
+        </>
+      )}
       <Flex center>
         <div className="service_panel__category_toggle">
           <Button
